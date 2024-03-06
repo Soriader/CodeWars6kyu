146fd14cb536289296a87cf1ada7af34abe9e8c9
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-03-06 12:30:56 +0100

    [~] Replace With Alphabet Position

---
+++ CodeWars6kyu/AlphabetPositionTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6kyu
+{
+    public class AlphabetPositionTest
+    {
+        public static string AlphabetPosition(string text)
+        {
+            string result = "";
+
+            foreach (var letter in text.ToUpper())
+            {
+                if (char.IsLetter(letter))
+                {
+                    int number = letter - 'A' + 1;
+
+                    result += number.ToString() + " ";
+                }
+
+            }
+
+            return result.Trim();
+        }
+    }
+}
+//https://www.codewars.com/kata/546f922b54af40e1e90001da/train/csharp
