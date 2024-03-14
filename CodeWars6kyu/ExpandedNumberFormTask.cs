5c2a8ac114a9d0bd1195021a60d2c5da13e768a4
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-03-14 17:52:18 +0100

    [~] Task: Write Number in Expanded Form

---
+++ CodeWars6kyu/ExpandedNumberFormTask.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CodeWars6kyu
+{
+    public class ExpandedNumberFormTask
+    {
+        public static string ExpandedForm(long num)
+        {
+            if (num == 0)
+            {
+                return "0";
+            }
+
+            string result = "";
+
+            long remaining = num;
+            long divider = 1;
+
+            while (remaining > 0)
+            {
+                long digit = remaining % 10;
+                remaining /= 10;
+
+                if (digit > 0)
+                {
+                    result = $"{digit * divider} + {result}";
+                }
+
+                divider *= 10;
+            }
+
+            return result.Substring(0, result.Length - 3);
+        }
+    }
+}
+//https://www.codewars.com/kata/5842df8ccbd22792a4000245/train/csharp
--- CodeWars6kyuTest/UnitTest1.cs
+++ CodeWars6kyuTest/UnitTest1.cs
@@ -8,6 +8,14 @@ namespace CodeWars6kyuTest
         public class KataTest
         {
             [Test]
+            public void ExpandedNumberFormTest()
+            {
+                Assert.That(ExpandedNumberFormTask.ExpandedForm(12), Is.EqualTo("10 + 2"));
+                Assert.That(ExpandedNumberFormTask.ExpandedForm(42), Is.EqualTo("40 + 2"));
+                Assert.That(ExpandedNumberFormTask.ExpandedForm(70304), Is.EqualTo("70000 + 300 + 4"));
+            }
+
+            [Test]
             public void DuplicateEncoderTest()
             {
                 Assert.AreEqual("(((", DuplicateEncoderTask.DuplicateEncode("din"));
