c87fc09b5d1677401b511d254bf714aa754a04e2
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-04-19 11:27:38 +0200

    [~] Task: Tortoise racing

---
+++ CodeWars6kyu/TortoiseRacingTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6kyu
+{
+    public class TortoiseRacingTask
+    {
+        public static int[] Race(int v1, int v2, int g)
+        {
+            if(v1 >= v2)
+            {
+                return null;
+            }
+
+            int timeToCatchInSeconds = g * 3600 / (v2 - v1);
+
+            int hours = timeToCatchInSeconds / 3600;
+            int minutes = (timeToCatchInSeconds % 3600) / 60;
+            int seconds = timeToCatchInSeconds % 60;
+
+            return new int[] { hours, minutes, seconds };
+
+            
+        }
+    }
+}
+//https://www.codewars.com/kata/55e2adece53b4cdcb900006c/train/csharp
--- CodeWars6kyuTest/UnitTest1.cs
+++ CodeWars6kyuTest/UnitTest1.cs
@@ -7,6 +7,15 @@ namespace CodeWars6kyuTest
         [TestFixture]
         public class KataTest
         {
+
+            [Test]
+            public void TortoiseRacingTest()
+            {
+                Assert.AreEqual(new int[] { 0, 32, 18 }, TortoiseRacingTask.Race(720, 850, 70));
+                Assert.AreEqual(new int[] { 3, 21, 49 }, TortoiseRacingTask.Race(80, 91, 37));
+                Assert.AreEqual(new int[] { 2, 0, 0 }, TortoiseRacingTask.Race(80, 100, 40));
+            }
+
             [Test]
             public static void DataReverseTest()
             {
