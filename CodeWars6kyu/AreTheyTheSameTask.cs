28546a999d802e4b04980845837948afc3786656
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-06-02 21:12:57 +0200

    [~] Task: Are they the "same"?

---
+++ CodeWars6kyu/AreTheyTheSameTask.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6kyu
+{
+    public class AreTheyTheSameTask
+    {
+        public static bool Comp(int[] a, int[] b)
+        {
+
+            if (a == null || b == null || a.Length != b.Length)
+            {
+                return false;
+            }
+
+            Dictionary<int, int> squaresCount = new Dictionary<int, int>();
+
+            foreach (int num in a)
+            {
+                int square = num * num;
+                if (squaresCount.ContainsKey(square))
+                {
+                    squaresCount[square]++;
+                }
+                else
+                {
+                    squaresCount[square] = 1;
+                }
+            }
+
+            foreach (int num in b)
+            {
+                if (squaresCount.ContainsKey(num) && squaresCount[num] > 0)
+                {
+                    squaresCount[num]--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
+//https://www.codewars.com/kata/550498447451fbbd7600041c/train/csharp
--- CodeWars6kyuTest/UnitTest1.cs
+++ CodeWars6kyuTest/UnitTest1.cs
@@ -11,7 +11,15 @@ namespace CodeWars6kyuTest
             public class FrontTests
             {
 
-                
+                [Test]
+                public void AreTheyTheSameTest()
+                {
+                    int[] a = new int[] { 121, 144, 19, 161, 19, 144, 19, 11 };
+                    int[] b = new int[] { 11 * 11, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19 };
+                    bool r = AreTheyTheSameTask.Comp(a, b); 
+                    Assert.AreEqual(true, r);
+                }
+
                 [Test]
                 public void DecipherThisTest()
                 {
