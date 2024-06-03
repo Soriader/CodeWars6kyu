48409bb98a60e9a9975b33201a17fde65184cb50
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-06-03 22:16:11 +0200

    [~] Task: Statistics for an Athletic Association

---
+++ CodeWars6kyu/StatisticsForAnAthleticAssociationTask.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6kyu
+{
+    public class StatisticsForAnAthleticAssociationTask
+    {
+        public static string Stat(string strg)
+        {
+            if(strg == "")
+            {
+                return "";
+            }
+            var records = strg.Replace(" ", "").Replace('|', ':').Split(",").Select(x => TimeSpan.Parse(x)).OrderBy(x => x).ToList();
+
+            TimeSpan mediana;
+            var half = records.Count / 2;
+
+            if (records.Count % 2 == 0) 
+            { 
+                var average = (records[half - 1 ].Ticks + records[half].Ticks)/2;
+                mediana = TimeSpan.FromTicks(average);
+            }
+            else
+            {
+                mediana = records[half];
+            }
+
+
+
+            return $"Range: {(records.Max() - records.Min()).ToString(@"hh\|mm\|ss")} Average: {TimeSpan.FromTicks(records.Select(x => x.Ticks).Sum() / records.Count()).ToString(@"hh\|mm\|ss")} Median: {mediana.ToString(@"hh\|mm\|ss")}";
+
+
+        }
+    }
+}
+//https://www.codewars.com/kata/55b3425df71c1201a800009c/train/csharp
--- CodeWars6kyuTest/UnitTest1.cs
+++ CodeWars6kyuTest/UnitTest1.cs
@@ -10,6 +10,16 @@ namespace CodeWars6kyuTest
         {
             public class FrontTests
             {
+                [Test]
+                public static void StatisticsForAnAthleticAssociationTest()
+                {
+                    Assert.AreEqual("Range: 01|01|18 Average: 01|38|05 Median: 01|32|34",
+                            StatisticsForAnAthleticAssociationTask.Stat("01|15|59, 1|47|16, 01|17|20, 1|32|34, 2|17|17"));
+                    Assert.AreEqual("Range: 01|01|18 Average: 01|35|47 Median: 01|24|57",
+                            StatisticsForAnAthleticAssociationTask.Stat("01|15|59, 01|17|20, 1|32|34, 2|17|17"));
+                    Assert.AreEqual("Range: 00|31|17 Average: 02|26|18 Median: 02|22|00",
+                        StatisticsForAnAthleticAssociationTask.Stat("02|15|59, 2|47|16, 02|17|20, 2|32|34, 2|17|17, 2|22|00, 2|31|41"));
+                }
 
                 [Test]
                 public void AreTheyTheSameTest()
