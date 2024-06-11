fdc261505eed00298b2646fcfdc04a1dc45e83ed
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-06-11 19:23:16 +0200

    [~] Task: Meeting

---
+++ CodeWars6kyu/MeetingTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6kyu
+{
+    public class MeetingTask
+    {
+        public static string Meeting(string s)
+        {
+            var names = s.ToUpper().Split(';')
+            .Select(n => n.Split(':'))
+            .Select(n => (FirstName: n[0], LastName: n[1]))
+            .ToList();
+
+            var sortedNames = names
+                .OrderBy(n => n.LastName)
+                .ThenBy(n => n.FirstName)
+                .Select(n => $"({n.LastName}, {n.FirstName})");
+
+            return string.Join("", sortedNames);
+        }
+    }
+}
+//https://www.codewars.com/kata/59df2f8f08c6cec835000012/train/csharp
--- CodeWars6kyuTest/UnitTest1.cs
+++ CodeWars6kyuTest/UnitTest1.cs
@@ -10,6 +10,27 @@ namespace CodeWars6kyuTest
         {
             public class FrontTests
             {
+                [Test]
+                public static void MeetingTest()
+                {
+                    static void testing(string s, string expect)
+                    {
+                        string actual = MeetingTask.Meeting(s);
+                        Assert.AreEqual(expect, actual);
+                    }
+
+                    testing("Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill",
+                        "(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)");
+
+                    testing("Alexis:Wahl;John:Bell;Victoria:Schwarz;Abba:Dorny;Grace:Meta;Ann:Arno;Madison:Stan;Alex:Cornwell;Lewis:Kern;Megan:Stan;Alex:Korn",
+                        "(ARNO, ANN)(BELL, JOHN)(CORNWELL, ALEX)(DORNY, ABBA)(KERN, LEWIS)(KORN, ALEX)(META, GRACE)(SCHWARZ, VICTORIA)(STAN, MADISON)(STAN, MEGAN)(WAHL, ALEXIS)");
+
+                    testing("John:Gates;Michael:Wahl;Megan:Bell;Paul:Dorries;James:Dorny;Lewis:Steve;Alex:Meta;Elizabeth:Russel;Anna:Korn;Ann:Kern;Amber:Cornwell",
+                        "(BELL, MEGAN)(CORNWELL, AMBER)(DORNY, JAMES)(DORRIES, PAUL)(GATES, JOHN)(KERN, ANN)(KORN, ANNA)(META, ALEX)(RUSSEL, ELIZABETH)(STEVE, LEWIS)(WAHL, MICHAEL)");
+
+                    testing("Alex:Arno;Alissa:Cornwell;Sarah:Bell;Andrew:Dorries;Ann:Kern;Haley:Arno;Paul:Dorny;Madison:Kern",
+                        "(ARNO, ALEX)(ARNO, HALEY)(BELL, SARAH)(CORNWELL, ALISSA)(DORNY, PAUL)(DORRIES, ANDREW)(KERN, ANN)(KERN, MADISON)");
+                }
 
                 [Test]
                 public void ArrayHelpersTest()
