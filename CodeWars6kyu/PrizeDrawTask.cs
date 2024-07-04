4fd68aedd134f721c259cbb0a74cb2b3dae89c70
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-07-04 15:30:59 +0200

    [~] Task: Prize Draw

---
+++ CodeWars6kyu/PrizeDrawTask.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6kyu
+{
+    public class PrizeDrawTask
+    {
+        public static string NthRank(string st, int[] we, int n)
+        {
+
+            if (string.IsNullOrWhiteSpace(st)) return "No participants";
+
+            var names = st.Split(',');
+
+            if (names.Length < n) return "Not enough participants";
+
+            var participants = new List<Tuple<string, int>>();
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                var name = names[i];
+                var som = name.Length + name.ToLower().Sum(c => c - 'a' + 1);
+                var winningNumber = som * we[i];
+                participants.Add(Tuple.Create(name, winningNumber));
+            }
+
+            var sortedParticipants = participants
+                .OrderByDescending(p => p.Item2)
+                .ThenBy(p => p.Item1)
+                .ToList();
+
+            return sortedParticipants[n - 1].Item1;
+        }
+    }
+}
+//https://www.codewars.com/kata/5616868c81a0f281e500005c/train/csharp
--- CodeWars6kyuTest/UnitTest1.cs
+++ CodeWars6kyuTest/UnitTest1.cs
@@ -11,6 +11,26 @@ namespace CodeWars6kyuTest
             public class FrontTests
             {
                 [Test]
+                public static void PrizeDrawTest()
+                {
+                    string st = "";
+                    int[] we = new int[] { 4, 2, 1, 4, 3, 1, 2 };
+                    Assert.AreEqual("No participants", PrizeDrawTask.NthRank(st, we, 4));
+
+                    st = "Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin";
+                    we = new int[] { 4, 2, 1, 4, 3, 1, 2 };
+                    Assert.AreEqual("Not enough participants", PrizeDrawTask.NthRank(st, we, 8));
+
+                    st = "Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin";
+                    we = new int[] { 4, 2, 1, 4, 3, 1, 2 };
+                    Assert.AreEqual("Benjamin", PrizeDrawTask.NthRank(st, we, 4));
+
+                    st = "Elijah,Chloe,Elizabeth,Matthew,Natalie,Jayden";
+                    we = new int[] { 1, 3, 5, 5, 3, 6 };
+                    Assert.AreEqual("Matthew", PrizeDrawTask.NthRank(st, we, 2));
+                }
+
+                [Test]
                 public void ExtractPortionOfFileNameTest()
                 {
                     Assert.AreEqual("FILE_NAME.EXTENSION", ExtractPortionOfFileNameTask.ExtractFileName("1_FILE_NAME.EXTENSION.OTHEREXTENSIONadasdassdassds34"));
