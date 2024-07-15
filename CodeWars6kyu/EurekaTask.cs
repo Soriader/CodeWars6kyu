3e94e23e7ddad5ff52ee553c10ddb117a2333e02
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-07-15 20:08:08 +0200

    [~] Task: ake a Number And Sum Its Digits Raised To The Consecutive Powers And ....¡Eureka!!

---
+++ CodeWars6kyu/EurekaTask.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6kyu
+{
+    public class EurekaTask
+    {
+        public static long[] SumDigPow(long a, long b)
+        {
+            List<long> result = new List<long>();
+
+            for (long num = a; num <= b; num++)
+            {
+                if (IsEureka(num))
+                {
+                    result.Add(num);
+                }
+            }
+
+            return result.ToArray();
+        }
+
+        private static bool IsEureka(long num)
+        {
+            string numStr = num.ToString();
+            long sum = 0;
+
+            for (int i = 0; i < numStr.Length; i++)
+            {
+                int digit = (int)char.GetNumericValue(numStr[i]);
+                sum += (long)Math.Pow(digit, i + 1);
+            }
+
+            return sum == num;
+        }
+    }
+}
--- CodeWars6kyuTest/UnitTest1.cs
+++ CodeWars6kyuTest/UnitTest1.cs
@@ -11,6 +11,29 @@ namespace CodeWars6kyuTest
             public class FrontTests
             {
                 [Test]
+                public static void EurekaTest()
+                {
+                    static string Array2String(long[] list)
+                    {
+                        return "[" + string.Join(", ", list) + "]";
+                    }
+
+                    static void testing(long a, long b, long[] res)
+                    {
+                        Assert.AreEqual(Array2String(res), Array2String(EurekaTask.SumDigPow(a, b)));
+                    }
+
+                    testing(1, 10, new long[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+                    testing(1, 100, new long[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 89 });
+                    testing(10, 100, new long[] { 89 });
+                    testing(90, 100, new long[] { });
+                    testing(90, 150, new long[] { 135 });
+                    testing(50, 150, new long[] { 89, 135 });
+                    testing(10, 150, new long[] { 89, 135 });
+
+                }
+
+                [Test]
                 public void EnglishBeggarsTest()
                 {
                     int[] test = { 1, 2, 3, 4, 5 };
