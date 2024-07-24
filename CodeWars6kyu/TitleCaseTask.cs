6bbe765a4dd1c7999c6affb79e59c2fd33b89f10
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-07-24 18:20:02 +0200

    [~] Task: Title Case

--- CodeWars6kyu/TitleCaseTask.cs
+++ CodeWars6kyu/TitleCaseTask.cs
@@ -6,13 +6,29 @@ using System.Threading.Tasks;
 
 namespace CodeWars6kyu
 {
-    public class TitleCaseTask
-    {
-        public static string TitleCase(string title, string minorWords = "")
-        {
+	public class TitleCaseTask
+	{
+		public static string TitleCase(string title, string minorWords = "")
+		{
+			if (string.IsNullOrEmpty(title))
+			{
+				return title;
+			}
 
-            return "";
-        }
-    }
+			string[] minorWordsArray = string.IsNullOrEmpty(minorWords) ? new string[0] : minorWords.ToLower().Split(' ');
+
+			string[] words = title.ToLower().Split(' ');
+
+			for (int i = 0; i < words.Length; i++)
+			{
+				if (i == 0 || !minorWordsArray.Contains(words[i]))
+				{
+					words[i] = char.ToUpper(words[i][0]) + words[i].Substring(1);
+				}
+			}
+
+			return string.Join(" ", words);
+		}
+	}
 }
 //https://www.codewars.com/kata/5202ef17a402dd033c000009/train/csharp
--- CodeWars6kyuTest/UnitTest1.cs
+++ CodeWars6kyuTest/UnitTest1.cs
@@ -8,8 +8,14 @@ namespace CodeWars6kyuTest
         [TestFixture]
         public class KataTest
         {
-            public class FrontTests
+            public class Tests
             {
+				[Test]
+				public void TitleCaseTest()
+				{
+					Assert.AreEqual("The Quick Brown Fox", TitleCaseTask.TitleCase("the quick brown fox"));
+				}
+
 				[Test]
 				public void NewCashierDoesNotKnowTest()
 				{
@@ -94,7 +100,7 @@ namespace CodeWars6kyuTest
                     Assert.AreEqual(true, inrange);
                 }
 
-                static void test1()
+                static void Test1()
                 {
                         string data =
      "Rome:Jan 81.2,Feb 63.2,Mar 70.3,Apr 55.7,May 53.0,Jun 36.4,Jul 17.5,Aug 27.5,Sep 60.9,Oct 117.7,Nov 111.0,Dec 97.9" +
