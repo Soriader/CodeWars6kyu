f1b8a1159eb1d891c876fa673f05d19f30336a2a
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-09-27 18:54:03 +0200

    [~] Task: Easy Balance Checking

---
+++ CodeWars6kyu/EasyBalanceCheckingTask.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6kyu
+{
+	public class EasyBalanceCheckingTask
+	{
+		public static string Balance(string book)
+		{
+			var cleanBook = "";
+			foreach (char c in book)
+			{
+				if (char.IsLetterOrDigit(c) || c == '.' || c == ' ' || c == '\n')
+				{
+					cleanBook += c;
+				}
+			}
+
+			var partsOfBook = cleanBook.Split(new[] { ' ', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+			double totalExpense = 0;
+			int transactionCount = 0;
+			double originalBalance = 0;
+
+			try
+			{
+				originalBalance = double.Parse(partsOfBook[0], System.Globalization.CultureInfo.InvariantCulture);
+			}
+			catch (FormatException)
+			{
+				throw new FormatException("Initial balance format is incorrect!");
+			}
+
+			double currentBalance = originalBalance;
+			string result = $"Original Balance: {originalBalance.ToString("F2", System.Globalization.CultureInfo.InvariantCulture)}\n";
+
+			for (int i = 1; i < partsOfBook.Length; i += 3)
+			{
+				string checkNumber = partsOfBook[i];
+				string category = partsOfBook[i + 1];
+				double expense = 0;
+
+				try
+				{
+					expense = double.Parse(partsOfBook[i + 2], System.Globalization.CultureInfo.InvariantCulture);
+				}
+				catch (FormatException)
+				{
+					throw new FormatException($"Expense format is incorrect in check {checkNumber}.");
+				}
+
+				currentBalance -= expense;
+				result += $"{checkNumber} {category} {expense.ToString("F2", System.Globalization.CultureInfo.InvariantCulture)} Balance {currentBalance.ToString("F2", System.Globalization.CultureInfo.InvariantCulture)}\n";
+
+				totalExpense += expense;
+				transactionCount++;
+			}
+
+			double averageExpense = totalExpense / transactionCount;
+			result += $"Total expense  {totalExpense.ToString("F2", System.Globalization.CultureInfo.InvariantCulture)}\n";
+			result += $"Average expense  {averageExpense.ToString("F2", System.Globalization.CultureInfo.InvariantCulture)}";
+
+			return result;
+
+		}
+	}
+}
+//https://www.codewars.com/kata/59d727d40e8c9dd2dd00009f/train/csharp
--- CodeWars6kyuTest/UnitTest1.cs
+++ CodeWars6kyuTest/UnitTest1.cs
@@ -14,6 +14,32 @@ namespace CodeWars6kyuTest
             public class Tests
             {
 				[Test]
+				public static void EasyBalanceCheckingTest()
+				{
+					static void dotest(string s, string exp)
+					{
+						Console.Write("s:\n" + s + "\n");
+						string ans = EasyBalanceCheckingTask.Balance(s);
+						Console.Write("ACTUAL:\n" + ans + "\n");
+						Console.Write("EXPECT:\n" + exp + "\n");
+						Console.Write("{0:D}\n", exp == ans);
+						Assert.AreEqual(exp, ans);
+						Console.WriteLine("-");
+					}
+
+					String b1 = "1000.00!=\n125 Market !=:125.45\n126 Hardware =34.95\n127 Video! 7.45\n128 Book   :14.32\n129 Gasoline ::16.10";
+					String b1sol = "Original Balance: 1000.00\n125 Market 125.45 Balance 874.55\n126 Hardware 34.95 Balance 839.60\n127 Video 7.45 Balance 832.15\n128 Book 14.32 Balance 817.83\n129 Gasoline 16.10 Balance 801.73\nTotal expense  198.27\nAverage expense  39.65";
+					dotest(b1, b1sol);
+
+					String b2 = "1233.00\n125 Hardware;! 24.80?\n123 Flowers 93.50;\n127 Meat 120.90\n120 Picture 34.00\n124 Gasoline 11.00\n" +
+								"123 Photos;! 71.40?\n122 Picture 93.50\n132 Tyres;! 19.00,?;\n129 Stamps; 13.60\n129 Fruits{} 17.60\n129 Market;! 128.00?\n121 Gasoline;! 13.60?";
+					String b2sol = "Original Balance: 1233.00\n125 Hardware 24.80 Balance 1208.20\n123 Flowers 93.50 Balance 1114.70\n127 Meat 120.90 Balance 993.80\n120 Picture 34.00 Balance 959.80\n124 Gasoline 11.00 Balance 948.80\n123 Photos 71.40 Balance 877.40\n122 Picture 93.50 Balance 783.90\n132 Tyres 19.00 Balance 764.90\n129 Stamps 13.60 Balance 751.30\n129 Fruits 17.60 Balance 733.70\n129 Market 128.00 Balance 605.70\n121 Gasoline 13.60 Balance 592.10\nTotal expense  640.90\nAverage expense  53.41";
+					dotest(b2, b2sol);
+
+
+				}
+
+				[Test]
 				public void ErrorsHistogramTest()
 				{
 					Assert.AreEqual("u  3     ***\rw  4     ****\rx  6     ******\rz  6     ******",
