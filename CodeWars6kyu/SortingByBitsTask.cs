e6dd6c03732822e88da6b83e7324c4b64743fda9
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-10-01 12:14:53 +0200

    [~] Task: Sorting by bits

---
+++ CodeWars6kyu/SortingByBitsTask.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CodeWars6kyu
+{
+	public class SortingByBitsTask
+	{
+		public static int[] SortByBit(int[] array)
+		{
+			return array.OrderBy(x => Convert.ToString(x, 2).Count(c => c == '1'))
+						.ThenBy(x => x)
+						.ToArray();
+
+		}
+	}
+}
+//https://www.codewars.com/kata/59fa8e2646d8433ee200003f/train/csharp
--- CodeWars6kyuTest/UnitTest1.cs
+++ CodeWars6kyuTest/UnitTest1.cs
@@ -14,6 +14,14 @@ namespace CodeWars6kyuTest
             public class Tests
             {
 				[Test]
+				public void SortingByBitsTest()
+                {
+					Assert.AreEqual(new[] { 1, 8, 3, 3, 5, 6, 9, 7 }, SortingByBitsTask.SortByBit(new[] { 3, 8, 3, 6, 5, 7, 9, 1 }));
+					Assert.AreEqual(new[] { 0, 2, 2, 4, 8, 8, 3, 5, 5, 6, 9, 7, 56 }, SortingByBitsTask.SortByBit(new[] { 9, 4, 5, 3, 5, 7, 2, 56, 8, 2, 6, 8, 0 }));
+
+				}
+
+				[Test]
 				public void BrakingWellTest()
 				{
 					Assert.AreEqual(311.83146449201496, BrakingWellTask.Dist(144, 0.3), 1e-2);
