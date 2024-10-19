953ded4daa957059b2e3ca3f78e623c7fd3c571f
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-10-19 13:55:13 +0200

    [~] Task: Disease Spread

---
+++ CodeWars6kyu/DiseaseSpreadTask.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6kyu
+{
+	public class DiseaseSpreadTask
+	{
+		public static int Epidemic(int tm, int n, int s0, int i0, double b, double a)
+		{
+			double dt = (double)tm / n;
+
+			double S = s0;  
+			double I = i0;  
+			double R = 0;  
+
+			double maxInfected = I;
+
+			for (int k = 0; k < n; k++)
+			{
+				double newS = S - dt * b * S * I;
+				double newI = I + dt * (b * S * I - a * I);
+				double newR = R + dt * a * I;
+
+				S = newS;
+				I = newI;
+				R = newR;
+
+				if (I > maxInfected)
+				{
+					maxInfected = I;
+				}
+			}
+
+			return (int)maxInfected;
+		}
+
+	}
+}
+//https://www.codewars.com/kata/566543703c72200f0b0000c9/train/csharp
--- CodeWars6kyuTest/UnitTest1.cs
+++ CodeWars6kyuTest/UnitTest1.cs
@@ -14,6 +14,43 @@ namespace CodeWars6kyuTest
             public class Tests
             {
 				[Test]
+				public static void DiseaseSpreadTest()
+				{
+					static void testing(long act, long exp)
+					{
+						long r = Math.Abs(act - exp);
+						bool inrange = r <= 1;
+						if (inrange == false)
+						{
+							string specifier = "#.0";
+							Console.WriteLine("abs(actual - expected) must be <= 1 but was " + r.ToString(specifier));
+						}
+						Assert.AreEqual(true, inrange);
+					}
+
+					int tm = 18; int n = 432; int s0 = 1004; int i0 = 1; double b = 0.00209; double a = 0.51;
+					testing(DiseaseSpreadTask.Epidemic(tm, n, s0, i0, b, a), 420);
+					tm = 12; n = 288; s0 = 1007; i0 = 2; b = 0.00206; a = 0.45;
+					testing(DiseaseSpreadTask.Epidemic(tm, n, s0, i0, b, a), 461);
+					tm = 13; n = 312; s0 = 999; i0 = 1; b = 0.00221; a = 0.55;
+					testing(DiseaseSpreadTask.Epidemic(tm, n, s0, i0, b, a), 409);
+					tm = 24; n = 576; s0 = 1005; i0 = 1; b = 0.00216; a = 0.45;
+					testing(DiseaseSpreadTask.Epidemic(tm, n, s0, i0, b, a), 474);
+					tm = 24; n = 576; s0 = 982; i0 = 1; b = 0.00214; a = 0.44;
+					testing(DiseaseSpreadTask.Epidemic(tm, n, s0, i0, b, a), 460);
+					tm = 20; n = 480; s0 = 1000; i0 = 1; b = 0.00199; a = 0.53;
+					testing(DiseaseSpreadTask.Epidemic(tm, n, s0, i0, b, a), 386);
+					tm = 28; n = 672; s0 = 980; i0 = 1; b = 0.00198; a = 0.44;
+					testing(DiseaseSpreadTask.Epidemic(tm, n, s0, i0, b, a), 433);
+					tm = 14; n = 336; s0 = 996; i0 = 2; b = 0.00206; a = 0.41;
+					testing(DiseaseSpreadTask.Epidemic(tm, n, s0, i0, b, a), 483);
+					tm = 13; n = 312; s0 = 993; i0 = 2; b = 0.0021; a = 0.51;
+					testing(DiseaseSpreadTask.Epidemic(tm, n, s0, i0, b, a), 414);
+					tm = 28; n = 672; s0 = 999; i0 = 1; b = 0.00197; a = 0.55;
+					testing(DiseaseSpreadTask.Epidemic(tm, n, s0, i0, b, a), 368);
+				}
+
+				[Test]
 				public void WorldBitsWarTest()
 				{
 					DoTest(new List<int> { 1, 5, 12 }, "odds win");
