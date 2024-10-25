137211a241fa72446d89cc67ad1acb7705725e20
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-10-25 12:02:05 +0200

    [~] Task: Casino chips

---
+++ CodeWars6kyu/CasinoChipsTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6kyu
+{
+	public class CasinoChipsTask
+	{
+		public static int Solve(int[] arr)
+		{
+			Array.Sort(arr);
+			int result = 0;
+
+			while (arr[1] > 0 && arr[2] > 0)
+			{
+				arr[1]--;
+				arr[2]--;
+				result++;
+				Array.Sort(arr);
+			}
+
+			return result;
+		}
+
+	}
+}
+//https://www.codewars.com/kata/5e0b72d2d772160011133654/train/csharp
--- CodeWars6kyuTest/UnitTest1.cs
+++ CodeWars6kyuTest/UnitTest1.cs
@@ -14,6 +14,19 @@ namespace CodeWars6kyuTest
             public class Tests
             {
 				[Test]
+				public void CasinoChipsTest()
+				{
+					Assert.AreEqual(1, CasinoChipsTask.Solve(new int[] { 1, 1, 1 }));
+					Assert.AreEqual(2, CasinoChipsTask.Solve(new int[] { 1, 2, 1 }));
+					Assert.AreEqual(2, CasinoChipsTask.Solve(new int[] { 4, 1, 1 }));
+					Assert.AreEqual(9, CasinoChipsTask.Solve(new int[] { 8, 2, 8 }));
+					Assert.AreEqual(5, CasinoChipsTask.Solve(new int[] { 8, 1, 4 }));
+					Assert.AreEqual(10, CasinoChipsTask.Solve(new int[] { 7, 4, 10 }));
+					Assert.AreEqual(18, CasinoChipsTask.Solve(new int[] { 12, 12, 12 }));
+					Assert.AreEqual(3, CasinoChipsTask.Solve(new int[] { 1, 23, 2 }));
+				}
+
+				[Test]
 				public void ConvertIntegerToWhitespaceFormatTest()
 				{
 					string unbleach(string ws)
