0658eacdcd66c61ffc51fd58f49a6b703580f884
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-12-07 16:57:20 +0100

    [~] Task: Esolang: MiniBitMove

---
+++ CodeWars6kyu/MiniBitMoveTask.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6kyu
+{
+	public class MiniBitMoveTask
+	{
+		public static bool[] Interpreter(string code, bool[] array)
+		{
+			int selector = 0; 
+			int programLength = code.Length;
+
+			while (selector < array.Length)
+			{
+				foreach (char command in code)
+				{
+					if (selector >= array.Length)
+					{
+						break;
+					}
+
+					if (command == '1')
+					{
+						array[selector] = !array[selector];
+					}
+					else if (command == '0')
+					{
+						selector++;
+					}
+				}
+			}
+
+			return array;
+		}
+	}
+}
+//https://www.codewars.com/kata/587c0138110b20624e000253/train/csharp
--- CodeWars6kyuTest/UnitTest1.cs
+++ CodeWars6kyuTest/UnitTest1.cs
@@ -15,6 +15,13 @@ namespace CodeWars6kyuTest
 			public class Tests
 			{
 				[Test]
+				public void MiniBitMoveTest()
+				{
+					Assert.AreEqual(new bool[] { false, false, true, true, false, true, false }, MiniBitMoveTask.Interpreter("10", new bool[] { true, true, false, false, true, false, true }));
+					Assert.AreEqual(new bool[] { false, true, false, true, false, true, false, true, false, true }, MiniBitMoveTask.Interpreter("100", new bool[] { true, true, true, true, true, true, true, true, true, true }));
+				}
+
+				[Test]
 				public void EasyDiagonalTest()
 				{
 					Assert.AreEqual(new BigInteger(20349), EasyDiagonalTask.Diagonal(20, 4));
