47212326f55a232085077f67cec8e00ea54a4cc4
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-12-09 18:22:57 +0100

    [~] Task: Group in 10s

---
+++ CodeWars6kyu/GroupIn10sTask.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6kyu
+{
+	public class GroupIn10sTask
+	{
+		public static int[][] GroupIn10s(int[] array)
+		{
+			if (array == null || array.Length == 0)
+			{
+				return Array.Empty<int[]>();
+			}
+
+			Array.Sort(array);
+
+			int maxGroup = array.Max() / 10;
+
+			var grouped = new List<int[]>();
+
+			for (int i = 0; i <= maxGroup; i++)
+			{
+				var group = array.Where(num => num / 10 == i).ToArray();
+
+				grouped.Add(group.Length > 0 ? group : null);
+			}
+
+			return grouped.ToArray();
+		}
+	}
+}
+//https://www.codewars.com/kata/5694d22eb15d78fe8d00003a/train/csharp
--- CodeWars6kyuTest/UnitTest1.cs
+++ CodeWars6kyuTest/UnitTest1.cs
@@ -15,6 +15,21 @@ namespace CodeWars6kyuTest
 			public class Tests
 			{
 				[Test]
+				public void GroupIn10sTest()
+				{
+					var expected = new int[][] { new int[] { 1, 2, 3 } };
+					Assert.AreEqual(expected, GroupIn10sTask.GroupIn10s(new int[] { 1, 2, 3 }));
+
+					expected = new int[][] { new int[] { 3, 8 },
+							   new int[] { 12, 17, 19 },
+							   new int[] { 25 },
+							   new int[] { 35, 38 },
+							   null,
+							   new int[] { 50 } };
+					Assert.AreEqual(expected, GroupIn10sTask.GroupIn10s(new int[] { 8, 12, 38, 3, 17, 19, 25, 35, 50 }));
+				}
+
+				[Test]
 				public void SimpleDivisionTest()
 				{
 					Assert.AreEqual(true, SimpleDivisionTask.Solve(2, 256));
