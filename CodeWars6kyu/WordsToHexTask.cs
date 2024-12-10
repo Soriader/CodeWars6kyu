86bf1a318e4e3a49c42781c04514879118210c0b
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-12-10 15:37:28 +0100

    [~] Task: Words to Hex

---
+++ CodeWars6kyu/WordsToHexTask.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6kyu
+{
+	public class WordsToHexTask
+	{
+		public static string[] WordsToHex(string words)
+		{
+			var partOfWords = words.Split(' ');
+			List<string> result = new List<string>();
+
+			foreach (var word in partOfWords)
+			{
+				var wordToChange = word.PadRight(3, '\0');
+
+				string r = ((int)wordToChange[0]).ToString("X2").ToLower();
+				string g = ((int)wordToChange[1]).ToString("X2").ToLower();
+				string b = ((int)wordToChange[2]).ToString("X2").ToLower();
+
+				result.Add($"#{r}{g}{b}");
+			}
+
+			return result.ToArray();
+		}
+	}
+}
+//https://www.codewars.com/kata/596e91b48c92ceff0c00001f/train/csharp
--- CodeWars6kyuTest/UnitTest1.cs
+++ CodeWars6kyuTest/UnitTest1.cs
@@ -15,6 +15,16 @@ namespace CodeWars6kyuTest
 			public class Tests
 			{
 				[Test]
+				public void WordsToHexTest()
+				{
+					Assert.AreEqual(new string[] { "#48656c", "#6d7900", "#6e616d", "#697300", "#476172", "#616e64", "#490000", "#6c696b", "#636865" }, WordsToHexTask.WordsToHex("Hello, my name is Gary and I like cheese."));
+					Assert.AreEqual(new string[] { "#303132" }, WordsToHexTask.WordsToHex("0123456789"));
+					Assert.AreEqual(new string[] { "#546869" }, WordsToHexTask.WordsToHex("ThisIsOneLongSentenceThatConsistsOfWords"));
+					Assert.AreEqual(new string[] { "#426c61", "#626c61", "#626c61", "#626c61" }, WordsToHexTask.WordsToHex("Blah blah blah blaaaaaaaaaaaah"));
+					Assert.AreEqual(new string[] { "#262626", "#242424", "#5e5e5e", "#404040", "#282928" }, WordsToHexTask.WordsToHex("&&&&& $$$$$ ^^^^^ @@@@@ ()()()()("));
+				}
+
+				[Test]
 				public void GA_DE_RY_PO_LU_KICypherTest()
 				{
 					Assert.AreEqual("Ala has a cat", GA_DE_RY_PO_LU_KICypherTask.Encode("Gug hgs g cgt", "gaderypoluki"));
