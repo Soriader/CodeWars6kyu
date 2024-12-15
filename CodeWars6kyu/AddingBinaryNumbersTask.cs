93446cbe200a3ab8b8fb4c4603938025ba0d09ee
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-12-15 19:57:23 +0100

    [~] Task: Adding Binary Numbers

---
+++ CodeWars6kyu/AddingBinaryNumbersTask.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CodeWars6kyu
+{
+	public class AddingBinaryNumbersTask
+	{
+		//In this task I can't use Convert method
+		public static string Add(string a, string b)
+		{
+			var resultInInt = ConvertToInt32(a) + ConvertToInt32(b);
+
+			string binary = "";
+
+			if (resultInInt == 0)
+			{
+				binary = "0";
+			}
+			else
+			{
+				while (resultInInt > 0)
+				{
+					binary = (resultInInt % 2) + binary; 
+					resultInInt /= 2;
+				}
+			}
+
+			return binary;
+
+		}
+
+		private static int ConvertToInt32(string s) 
+		{
+			int result = 0;
+			for (int i = 0; i < s.Length; i++)
+			{
+				result = (result << 1) + (s[i] - '0');
+			}
+
+			return result;
+		}
+	}
+}
+//https://www.codewars.com/kata/55c11989e13716e35f000013/train/csharp
--- CodeWars6kyuTest/UnitTest1.cs
+++ CodeWars6kyuTest/UnitTest1.cs
@@ -15,6 +15,17 @@ namespace CodeWars6kyuTest
 			public class Tests
 			{
 				[Test]
+				public void AddingBinaryNumbersTest()
+				{
+					Assert.AreEqual("1001", AddingBinaryNumbersTask.Add("111", "10"));
+					Assert.AreEqual("10010", AddingBinaryNumbersTask.Add("1101", "101"));
+					Assert.AreEqual("100100", AddingBinaryNumbersTask.Add("1101", "10111"));
+					Assert.AreEqual("11", AddingBinaryNumbersTask.Add("0011", "00"));
+					Assert.AreEqual("11", AddingBinaryNumbersTask.Add("00", "0011"));
+				}
+
+
+				[Test]
 				public void PossibilitiesOfThrowingACoinTest()
 				{
 					PossibilitiesOfThrowingACoinTask c = new PossibilitiesOfThrowingACoinTask();
