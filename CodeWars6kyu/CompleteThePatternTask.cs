94525fdd6541602d6667b41b8eb50672a50d6699
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-12-19 20:02:04 +0100

    [~] Task: Complete The Pattern #12

---
+++ CodeWars6kyu/CompleteThePatternTask.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6kyu
+{
+	public class CompleteThePatternTask
+	{
+		public static string Pattern(int n)
+		{
+			if (n < 1)
+			{
+				return "";
+			}
+
+			var result = new List<string>();
+			int totalLength = 2 * n - 1; 
+
+			for (int i = 1; i <= n; i++)
+			{
+				int frontBackspace = i - 1;
+				int innerSpace = totalLength - 2 - 2 * frontBackspace;
+
+				if (innerSpace > 0)
+				{
+					result.Add($"{new string(' ', frontBackspace)}{i % 10}{new string(' ', innerSpace)}{i % 10}{new string(' ', frontBackspace)}");
+				}
+				else
+				{
+					result.Add($"{new string(' ', frontBackspace)}{i % 10}{new string(' ', frontBackspace)}");
+				}
+			}
+
+			for (int i = n - 1; i >= 1; i--)
+			{
+				int frontBackspace = i - 1;
+				int innerSpace = totalLength - 2 - 2 * frontBackspace;
+
+				if (innerSpace > 0)
+				{
+					result.Add($"{new string(' ', frontBackspace)}{i % 10}{new string(' ', innerSpace)}{i % 10}{new string(' ', frontBackspace)}");
+				}
+				else
+				{
+					result.Add($"{new string(' ', frontBackspace)}{i % 10}{new string(' ', frontBackspace)}");
+				}
+			}
+
+			return string.Join("\n", result);
+		}
+
+
+	}
+}
+//https://www.codewars.com/kata/558ac25e552b51dbc60000c3/train/csharp
--- CodeWars6kyuTest/UnitTest1.cs
+++ CodeWars6kyuTest/UnitTest1.cs
@@ -15,6 +15,13 @@ namespace CodeWars6kyuTest
 			public class Tests
 			{
 				[Test]
+				public void CompleteThePatternTest()
+				{
+					//Assert.AreEqual("1   1\n 2 2 \n  3  \n 2 2 \n1   1", CompleteThePatternTask.Pattern(3));
+					Assert.AreEqual("1       1\n 2     2 \n  3   3  \n   4 4   \n    5    \n   4 4   \n  3   3  \n 2     2 \n1       1", CompleteThePatternTask.Pattern(5));
+				}
+
+				[Test]
 				public void MakeAWindowTest()
 				{
 					Assert.AreEqual("-----\n|.|.|\n|-+-|\n|.|.|\n-----", MakeAWindowTask.MakeAWindow(1));
