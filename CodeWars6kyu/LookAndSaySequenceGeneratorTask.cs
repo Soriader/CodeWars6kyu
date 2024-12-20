9707737db8ca1b8aace4e809814b74a21cf9ec03
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-12-20 23:47:08 +0100

    [~] Task: Look and say sequence generator

---
+++ CodeWars6kyu/LookAndSaySequenceGeneratorTask.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6kyu
+{
+    public class LookAndSaySequenceGeneratorTask
+    {
+        public static string LookAndSaySequence(string firstElement, long n)
+        {
+            if (n == 1)
+            {
+                return firstElement;
+            }
+
+            var sequence = firstElement;
+
+            for (int iterator = 1; iterator < n; iterator++)
+            {
+                string boxOfSequence = "";
+                int i = 0;
+
+                while (i < sequence.Length)
+                {
+                    char currentChar = sequence[i];
+                    int countElement = 0;
+
+                    while (i < sequence.Length && sequence[i] == currentChar)
+                    {
+                        countElement++;
+                        i++;
+                    }
+
+                    boxOfSequence += $"{countElement}{currentChar}";
+                }
+
+                sequence = boxOfSequence;
+            }
+
+            return sequence;
+        }
+    }
+}
+//https://www.codewars.com/kata/592421cb7312c23a990000cf/train/csharp
--- CodeWars6kyuTest/UnitTest1.cs
+++ CodeWars6kyuTest/UnitTest1.cs
@@ -14,7 +14,17 @@ namespace CodeWars6kyuTest
 		{
 			public class Tests
 			{
-				[Test]
+                [Test]
+                public void LookAndSaySequenceGeneratorTest()
+                {
+                    //Assert.AreEqual("1", LookAndSaySequenceGeneratorTask.LookAndSaySequence("1", 1));
+                    //Assert.AreEqual("21", LookAndSaySequenceGeneratorTask.LookAndSaySequence("1", 3));
+                    Assert.AreEqual("111221", LookAndSaySequenceGeneratorTask.LookAndSaySequence("1", 5));
+                    Assert.AreEqual("22", LookAndSaySequenceGeneratorTask.LookAndSaySequence("22", 10));
+                    Assert.AreEqual("1114", LookAndSaySequenceGeneratorTask.LookAndSaySequence("14", 2));
+                }
+
+                [Test]
 				public void CompleteThePatternTest()
 				{
 					//Assert.AreEqual("1   1\n 2 2 \n  3  \n 2 2 \n1   1", CompleteThePatternTask.Pattern(3));
