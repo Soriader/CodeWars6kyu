643c417ca818d7870cb7ebdfcdf144a6dbd595ff
NewLavoisier <kacperjwawrzyniak@gmail.com>
2024-12-21 18:35:57 +0100

    [~] Task: Hollow array

---
+++ CodeWars6kyu/HollowArrayTask.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6kyu
+{
+    public class HollowArrayTask
+    {
+        public static bool IsHollow(int[] x)
+        {
+            int firstZeroIndex = Array.IndexOf(x, 0);
+            int lastZeroIndex = Array.LastIndexOf(x, 0);
+
+            if (lastZeroIndex - firstZeroIndex + 1 < 3)
+            {
+                return false;
+
+            }
+
+            for (int i = firstZeroIndex; i <= lastZeroIndex; i++)
+            {
+                if (x[i] != 0)
+                {
+                    return false;
+
+                }
+            }
+
+            int leftNonZeros = firstZeroIndex;
+            int rightNonZeros = x.Length - lastZeroIndex - 1;
+
+            return leftNonZeros == rightNonZeros;
+        }
+    }
+}
+//https://www.codewars.com/kata/59b72376460387017e000062/train/csharp
--- CodeWars6kyuTest/UnitTest1.cs
+++ CodeWars6kyuTest/UnitTest1.cs
@@ -15,10 +15,17 @@ namespace CodeWars6kyuTest
 			public class Tests
 			{
                 [Test]
+                public void HollowArrayTest()
+                {
+                    Assert.AreEqual(true, HollowArrayTask.IsHollow(new int[] { -1, 0, 0, 0, 3 }));
+                    Assert.AreEqual(false, HollowArrayTask.IsHollow(new int[] { -1, 0, 0, 0, 0 }));
+                }
+
+                [Test]
                 public void LookAndSaySequenceGeneratorTest()
                 {
-                    //Assert.AreEqual("1", LookAndSaySequenceGeneratorTask.LookAndSaySequence("1", 1));
-                    //Assert.AreEqual("21", LookAndSaySequenceGeneratorTask.LookAndSaySequence("1", 3));
+                    Assert.AreEqual("1", LookAndSaySequenceGeneratorTask.LookAndSaySequence("1", 1));
+                    Assert.AreEqual("21", LookAndSaySequenceGeneratorTask.LookAndSaySequence("1", 3));
                     Assert.AreEqual("111221", LookAndSaySequenceGeneratorTask.LookAndSaySequence("1", 5));
                     Assert.AreEqual("22", LookAndSaySequenceGeneratorTask.LookAndSaySequence("22", 10));
                     Assert.AreEqual("1114", LookAndSaySequenceGeneratorTask.LookAndSaySequence("14", 2));
