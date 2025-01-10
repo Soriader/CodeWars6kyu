55cf0c05ea1dd147d61895ea61b11e34e11313f4
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-01-10 18:16:53 +0100

    [~] Task: Message from Aliens

---
+++ CodeWars6kyu/MessageFromAliensTask.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6kyu
+{
+    public class MessageFromAliensTask
+    {
+        private static Dictionary<string, string> AlienLanguage = new Dictionary<string, string>() {
+          { "/\\","a" }
+         ,{ "]3","b" }
+         ,{ "(","c" }
+         ,{ "|)","d" }
+         ,{ "[-","e"}
+         ,{ "/=","f" }
+         ,{ "(_,","g" }
+         ,{ "|-|","h"}
+         ,{ "|","i"}
+         ,{ "_T","j"}
+         ,{ "/<","k" }
+         ,{ "|_","l"  }
+         ,{ "|\\/|","m"}
+         ,{ "|\\|","n" }
+         ,{ "()","o"  }
+         ,{ "|^","p"}
+         ,{ "()_","q"}
+         ,{ "/?","r"  }
+         ,{ "_\\~","s"}
+         ,{ "~|~","t"}
+         ,{ "|_|","u" }
+         ,{ "\\/","v"  }
+         ,{ "\\/\\/","w"}
+         ,{ "`/","y"}
+         ,{ "><","x"}
+         ,{ "~/_","z"}
+         ,{ "__"," " }
+  };
+        public static string Decode(string m)
+        {
+            return string.Concat(m.Split(m[0]).Where(e => e.Length > 0).Select(c => AlienLanguage[c]).Reverse());
+
+        }
+    }
+}
+//https://www.codewars.com/kata/598980a41e55117d93000015/train/csharp
--- CodeWars6kyuTest/UnitTest1.cs
+++ CodeWars6kyuTest/UnitTest1.cs
@@ -15,6 +15,16 @@ namespace CodeWars6kyuTest
 			public class Tests
 			{
                 [Test]
+                public void MessageFromAliensTest()
+                {
+                    Assert.That(MessageFromAliensTask.Decode(@"]()]|_]]|_]][-]|-|]"), Is.EqualTo("hello"));
+                    Assert.That(MessageFromAliensTask.Decode("{|^{|{{|_{]3{"), Is.EqualTo("blip"));
+                    Assert.That(MessageFromAliensTask.Decode("..[-.|_.|^....().[-.|^.__..|)...|.|^.|_|..~|~._\\~.__...[-..|.|).."), Is.EqualTo("die stupid people"));
+                    Assert.That(MessageFromAliensTask.Decode("'''_\\~'|_|'()'|''('|'|_'[-'|)''__'_\\~'/<'()'()'|_'''__'|\\|'|''/\\'/?']3'__''/?'|_|''()'`/''"), Is.EqualTo("your brain looks delicious"));
+                    Assert.That(MessageFromAliensTask.Decode("}/\\}~|~}/\\}/<}__}|)}}}[-}~|~}/\\}(}|}|_}|^}|_|}|)}__}|)}}}|\\|}|}/=}__}()}}}~|~}__}`/}/?}}~|~}"), Is.EqualTo("try to find duplicated kata"));
+                }
+
+                [Test]
                 public void ParsingTimeTest()
                 {
 					Assert.AreEqual(0, ParsingTimeTask.ToSeconds("00:00:00"));
