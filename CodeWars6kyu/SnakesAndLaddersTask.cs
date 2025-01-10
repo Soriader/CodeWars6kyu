2e8f826bd30d7feaf3262585f6b6083184f27ba6
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-01-10 14:52:20 +0100

    [~] Task: Snakes & Ladders

---
+++ CodeWars6kyu/SnakesAndLaddersTask.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6kyu
+{
+    public class SnakesAndLaddersTask
+    {
+        public static int SnakesAndLadders(int[] board, int[] dice)
+        {
+            int position = 0;
+
+            for (int i = 0; i < dice.Length; i++)
+            {
+                if (position + dice[i] + 1 > board.Length)
+                {
+                    continue;
+                }
+                else
+                {
+                    position += dice[i];
+                    position += board[position];
+                }
+            }
+
+            return position;
+        }
+    }
+}
+//https://www.codewars.com/kata/5821cd4770ca285b1f0001d5/train/csharp
--- CodeWars6kyuTest/UnitTest1.cs
+++ CodeWars6kyuTest/UnitTest1.cs
@@ -14,7 +14,25 @@ namespace CodeWars6kyuTest
 		{
 			public class Tests
 			{
-                [Test, Order(1)]
+
+                [Test]
+                public void SnakesAndLaddersTest()
+                {
+                    var dice = new[] { 2, 1, 5, 1, 5, 4 };
+                    var board = new[] { 0, 0, 3, 0, 0, 0, 0, -2, 0, 0, 0 };
+                    //Assert.That(SnakesAndLaddersTask.SnakesAndLadders(board, dice), Is.EqualTo(10));
+
+                    dice = new[] { 1, 4, 2, 7, 1, 3, 5, 7, 1 };
+                    board = new[] { 0, 0, 0, 0, 0, 0, 0, -1, 9, 0, 0, 0, 0, 0, -13, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+                    Assert.That(SnakesAndLaddersTask.SnakesAndLadders(board, dice), Is.EqualTo(17));
+
+                    dice = new[] { 3, 1, 5, 2, 1, 6, 4, 2, 6 };
+                    board = new[] { 0, 0, 0, 0, 4, 0, 4, -2, 0, 5, 0, 0, -9, 0, 0, -2, 0, 0, 0, 0, 0, 4, 0, -1, 0, 0, 0, 1, 0, 0 };
+                    Assert.That(SnakesAndLaddersTask.SnakesAndLadders(board, dice), Is.EqualTo(26));
+
+                }
+
+                [Test]
                 public static void ShorterPathTest()
                 {
                     string[] goal = new string[8] { "N", "N", "N", "N", "N", "E", "N", "N" };
