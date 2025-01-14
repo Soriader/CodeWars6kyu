f77d43dfd1c942c691576da2bc465f1bd8a588f4
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-01-14 18:10:03 +0100

    [~] Task: Number of measurements to spot the counterfeit coin

---
+++ CodeWars6kyu/NumberOfMeasurementsCoinTask.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6kyu
+{
+    public class NumberOfMeasurementsCoinTask
+    {
+        public static long HowManyMeasurements(long n)
+        {
+            int result = 0;
+            int power = 1;
+
+            while (power < n)
+            {
+                result++;
+                power *= 3;
+            }
+
+            return result;
+        }
+    }
+}
+//https://www.codewars.com/kata/59530d2401d6039f8600001f/train/csharp
--- CodeWars6kyuTest/UnitTest1.cs
+++ CodeWars6kyuTest/UnitTest1.cs
@@ -13,7 +13,16 @@ namespace CodeWars6kyuTest
 		public class KataTest
 		{
 			public class Tests
-			{
+            {
+                [Test]
+                public void NumberOfMeasurementsCoinTest()
+                {
+                    Assert.That(NumberOfMeasurementsCoinTask.HowManyMeasurements(1), Is.EqualTo(0));
+                    Assert.That(NumberOfMeasurementsCoinTask.HowManyMeasurements(2), Is.EqualTo(1));
+                    Assert.That(NumberOfMeasurementsCoinTask.HowManyMeasurements(3), Is.EqualTo(1));
+                    Assert.That(NumberOfMeasurementsCoinTask.HowManyMeasurements(8), Is.EqualTo(2));
+                    Assert.That(NumberOfMeasurementsCoinTask.HowManyMeasurements(100), Is.EqualTo(5));
+                }
                 [Test]
                 public void SimpleSumOfPairsTest()
                 {
