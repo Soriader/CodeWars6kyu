7e2adf4aec3c8879242ed256c413d415263f0838
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-01-17 17:25:17 +0100

    [~] Task: String Pyramid

---
+++ CodeWars6kyu/StringPyramidTask.cs
@@ -0,0 +1,108 @@
+﻿using CodeWars6kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6kyu
+{
+    public class StringPyramidTask
+    {
+        public static string WatchPyramidFromTheSide(string characters)
+        {
+            if (string.IsNullOrEmpty(characters))
+            { 
+                return characters; 
+            }
+
+            string pyramid = "";
+            var baseOfPyramid = characters.Length * 2 - 1; 
+            int iterator = 0;
+            var blockForPyramid = characters.Reverse().ToArray();
+            int iteratorForBlock = 1;
+            int iteratorForSpaces = 1;
+
+            while (iterator != characters.Length)
+            {
+                pyramid += string.Concat(Enumerable.Repeat(" ", (baseOfPyramid - iteratorForSpaces) / 2)) 
+                    + string.Concat(Enumerable.Repeat(blockForPyramid[iterator], iteratorForBlock)) 
+                    + string.Concat(Enumerable.Repeat(" ", (baseOfPyramid - iteratorForSpaces) / 2)) 
+                    + "\n";
+
+                iteratorForBlock += 2;
+                iteratorForSpaces+=2;
+                iterator++;
+            }
+
+            return pyramid.TrimEnd('\n');
+        } 
+
+        public static string WatchPyramidFromAbove(string characters)
+        {
+            if (string.IsNullOrEmpty(characters))
+            {
+                return characters;
+            }
+
+            int n = characters.Length;
+            int baseWidth = n * 2 - 1;
+
+            char[][] pyramid = new char[baseWidth][];
+            for (int i = 0; i < baseWidth; i++)
+            {
+                pyramid[i] = new char[baseWidth];
+                Array.Fill(pyramid[i], characters[0]);
+            }
+
+            for (int i = 1; i < n; i++)
+            {
+                int start = i;
+                int end = baseWidth - i - 1;
+
+                for (int j = start; j <= end; j++)
+                {
+                    pyramid[start][j] = characters[i];
+                    pyramid[end][j] = characters[i];
+                    pyramid[j][start] = characters[i];
+                    pyramid[j][end] = characters[i];
+                }
+            }
+
+            return string.Join("\n", pyramid.Select(row => new string(row)));
+        }
+
+        public static int CountVisibleCharactersOfThePyramid(string characters)
+        {
+            if (string.IsNullOrEmpty(characters))
+            {
+                return -1;
+            }
+
+            int n = characters.Length;
+            int baseWidth = n * 2 - 1;
+
+            return baseWidth * baseWidth;
+        }
+
+        public static int CountAllCharactersOfThePyramid(string characters)
+        {
+            if (string.IsNullOrEmpty(characters))
+            {
+                return -1;
+            }
+
+            int n = characters.Length;
+            int totalCharacters = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int layerWidth = i * 2 - 1;
+                totalCharacters += layerWidth * layerWidth;
+            }
+
+            return totalCharacters;
+        }
+    }
+}
+//https://www.codewars.com/kata/5797d1a9c38ec2de1f00017b/train/csharp
--- CodeWars6kyuTest/UnitTest1.cs
+++ CodeWars6kyuTest/UnitTest1.cs
@@ -14,6 +14,78 @@ namespace CodeWars6kyuTest
 		{
 			public class Tests
             {
+                //[Test]
+                //public void StringPyramidTestForFirstPart()
+                //{
+                //                string expectedWatchFromSide ="  c  \n" +" bbb \n" +"aaaaa";
+                //                Assert.AreEqual(expectedWatchFromSide, StringPyramidTask.WatchPyramidFromTheSide("abc"));
+
+                //                expectedWatchFromSide = "      7      \n"
+                //    + "     666     \n"
+                //    + "    55555    \n"
+                //    + "   4444444   \n"
+                //    + "  333333333  \n"
+                //    + " 22222222222 \n"
+                //    + "1111111111111";
+
+                //                Assert.AreEqual(expectedWatchFromSide, StringPyramidTask.WatchPyramidFromTheSide("1234567"));
+                //            }
+
+                [Test]
+                public void StringPyramidTestForSecondPart()
+				{
+                    string expectedWatchFromAbove =
+                    "aaaaa\n" +
+                    "abbba\n" +
+                    "abcba\n" +
+                    "abbba\n" +
+                    "aaaaa";
+                    Assert.AreEqual(expectedWatchFromAbove, StringPyramidTask.WatchPyramidFromAbove("abc"));
+
+                    expectedWatchFromAbove =
+    "7777777\n" +
+    "7666667\n" +
+    "7655567\n" +
+    "7654567\n" +
+    "7655567\n" +
+    "7666667\n" +
+    "7777777";
+
+                    Assert.AreEqual(expectedWatchFromAbove, StringPyramidTask.WatchPyramidFromAbove("1234567"));
+
+                }
+
+                [Test]
+                public void StringPyramidTest()
+                {
+                    void Visualisation(string expectedWatchFromSide, string expectedWatchFromAbove, string actualWatchFromSide, string actualWatchFromAbove)
+                    {
+                        Console.WriteLine("From side correct:\n" + expectedWatchFromSide);
+                        Console.WriteLine("From above correct:\n" + expectedWatchFromAbove);
+                        Console.WriteLine("From side yours:\n" + actualWatchFromSide);
+                        Console.WriteLine("From above yours:\n" + actualWatchFromAbove);
+                        Assert.That(actualWatchFromSide, Is.EqualTo(expectedWatchFromSide));
+                        Assert.That(actualWatchFromAbove, Is.EqualTo(expectedWatchFromAbove));
+                    }
+
+                    string characters = "abc";
+                    string expectedWatchFromSide =
+                    "  c  \n" +
+                    " bbb \n" +
+                    "aaaaa";
+                    string expectedWatchFromAbove =
+                    "aaaaa\n" +
+                    "abbba\n" +
+                    "abcba\n" +
+                    "abbba\n" +
+                    "aaaaa";
+                    var actualWatchFromSide = StringPyramidTask.WatchPyramidFromTheSide(characters);
+                    var actualWatchFromAbove = StringPyramidTask.WatchPyramidFromAbove(characters);
+                    Visualisation(expectedWatchFromSide, expectedWatchFromAbove, actualWatchFromSide, actualWatchFromAbove);
+                    Assert.That(StringPyramidTask.CountVisibleCharactersOfThePyramid(characters), Is.EqualTo(25));
+                    Assert.That(StringPyramidTask.CountAllCharactersOfThePyramid(characters), Is.EqualTo(35));
+                }
+
                 [Test]
                 public void NumberOfMeasurementsCoinTest()
                 {
