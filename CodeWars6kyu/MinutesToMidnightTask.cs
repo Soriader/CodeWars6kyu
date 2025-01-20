bb0360f2f1999408d3a5f8f28a6de99297fe2bb8
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-01-20 18:15:12 +0100

    [~] Task: Minutes to Midnight

---
+++ CodeWars6kyu/MinutesToMidnightTask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6kyu
+{
+    public class MinutesToMidnightTask
+    {
+        public string CountMinutes(DateTime d)
+        {
+            DateTime midnight = d.Date.AddDays(1);
+            TimeSpan timeUntilMidnight = midnight - d;
+
+            int minutes = (int)Math.Round(timeUntilMidnight.TotalMinutes);
+
+            return minutes == 1 ? $"{minutes} minute" : $"{minutes} minutes";
+
+        }
+    }
+}
+//https://www.codewars.com/kata/58528e9e22555d8d33000163/train/csharp
--- CodeWars6kyuTest/UnitTest1.cs
+++ CodeWars6kyuTest/UnitTest1.cs
@@ -15,6 +15,17 @@ namespace CodeWars6kyuTest
 			public class Tests
             {
                 [Test]
+                public void MinutesToMidnightTest()
+                {
+                    MinutesToMidnightTask m = new MinutesToMidnightTask();
+                    DateTime testDate = DateTime.Now.Date.AddHours(12);
+                    Assert.That(m.CountMinutes(testDate), Is.EqualTo("720 minutes"));
+
+                    testDate = DateTime.Now.Date.AddHours(23).AddMinutes(59);
+                    Assert.That(m.CountMinutes(testDate), Is.EqualTo("1 minute"));
+                }
+
+                [Test]
                 public void RequiredDataITest()
                 {
                     string AsString(object[] o)
