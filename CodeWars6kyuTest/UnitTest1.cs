a0b5898672ecaca51c7c504a11cd85b19ef8afda
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-01-21 22:18:32 +0100

    [~] Task: Dictionary Merge

--- CodeWars6kyuTest/UnitTest1.cs
+++ CodeWars6kyuTest/UnitTest1.cs
@@ -14,6 +14,29 @@ namespace CodeWars6kyuTest
 		{
 			public class Tests
             {
+				[Test]
+				public void DictionaryMergeTest()
+				{
+					var original1 = new Dictionary<string, int> { { "A", 1 }, { "B", 2 }, { "C", 3 } };
+					var original2 = new Dictionary<string, int> { { "A", 4 }, { "D", 5 } };
+
+					var merged = DictionaryMergeTask.Merge(original1, original2);
+					var expected = new Dictionary<string, int[]>
+	{
+		{ "A", new[] { 1, 4 } },
+		{ "B", new[] { 2 } },
+		{ "C", new[] { 3 } },
+		{ "D", new[] { 5 } }
+	};
+
+					// Porównanie zawartoœci
+					Assert.AreEqual(expected.Keys, merged.Keys);
+					foreach (var key in expected.Keys)
+					{
+						Assert.AreEqual(expected[key], merged[key]);
+					}
+				}
+
                 [Test]
                 public void MinutesToMidnightTest()
                 {
