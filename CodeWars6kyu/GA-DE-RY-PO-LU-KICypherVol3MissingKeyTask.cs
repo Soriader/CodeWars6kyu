bc1121bfd11b84812fd5c623d6ca10e4195f77f0
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-01-27 19:51:14 +0100

    [~] Task: GA-DE-RY-PO-LU-KI cypher vol 3 - Missing key

---
+++ CodeWars6kyu/GA-DE-RY-PO-LU-KICypherVol3MissingKeyTask.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars6kyu;
+
+public class GA_DE_RY_PO_LU_KICypherVol3MissingKeyTask
+{
+    public static string FindTheKey(string[] messages, string[] secrets)
+    {
+        List<string> cipher = new();
+        
+        for(int i  = 0; i < messages.Length; i++)
+        {
+            for ( int j = 0; j < messages[i].Length; j++ )
+            {
+                if ( messages[i][j] != secrets[i][j] )
+                {
+                    if( secrets[i][j] < messages[i][j] )
+                    {
+                        cipher.Add(secrets[i][j].ToString() + messages[i][j].ToString());
+                    }
+                    else
+                    {
+                        cipher.Add(messages[i][j].ToString() + secrets[i][j].ToString());
+                    }
+                }
+            }
+        }
+
+        cipher.Sort();
+        HashSet<string> result = new(cipher);
+        
+        return string.Join("", result);
+    }
+}
+//https://www.codewars.com/kata/592bdf59912f2209710000e9/train/csharp
---
+++ CodeWars6kyuTest/GA_DE_RY_PO_LU_KICypherVol3MissingKey.cs
@@ -0,0 +1,14 @@
+﻿using CodeWars6kyu;
+
+namespace CodeWars6kyuTest;
+
+public class GA_DE_RY_PO_LU_KICypherVol3MissingKey
+{
+    [Test]
+    public void GA_DE_RY_PO_LU_KICypherVol3MissingKeyTest()
+    {
+        string[] messages = { "dance on the table", "hide my beers", "scouts rocks" };
+        string[] secretes = { "egncd pn thd tgbud" ,"hked mr bddys" ,"scplts ypcis" };
+        Assert.That(GA_DE_RY_PO_LU_KICypherVol3MissingKeyTask.FindTheKey(messages, secretes), Is.EqualTo("agdeikluopry"));               
+    }
+}
