c0c7eb988ade004dbed137bba53f9e317371dc4f
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-01-29 21:24:16 +0100

    [~] Task: Password Maker

---
+++ CodeWars6kyu/PasswordMakerTask.cs
@@ -0,0 +1,67 @@
+﻿namespace CodeWars6kyu;
+
+public class PasswordMakerTask
+{
+    private static readonly Random Random = new Random();
+    public static string MakePassword(int len, bool upper, bool lower, bool digits)
+    {
+        var charPool = new List<char>();
+        if (upper) charPool.AddRange("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        if (lower) charPool.AddRange("abcdefghijklmnopqrstuvwxyz");
+        if (digits) charPool.AddRange("0123456789");
+
+        if (charPool.Count < len)
+        {
+            throw new ArgumentException("Error! The length of the password is too short!");
+
+        }
+
+        var password = new char[len];
+        var usedChars = new HashSet<char>();
+
+        int index = 0;
+        if (upper)
+        {
+            var upperChars = charPool.Where(char.IsUpper).ToList();
+            password[index++] = upperChars[Random.Next(upperChars.Count)];
+            usedChars.Add(password[index - 1]);
+        }
+        if (lower)
+        {
+            var lowerChars = charPool.Where(char.IsLower).ToList();
+            password[index++] = lowerChars[Random.Next(lowerChars.Count)];
+            usedChars.Add(password[index - 1]);
+        }
+        if (digits)
+        {
+            var digitChars = charPool.Where(char.IsDigit).ToList();
+            password[index++] = digitChars[Random.Next(digitChars.Count)];
+            usedChars.Add(password[index - 1]);
+        }
+
+        while (index < len)
+        {
+            char randomChar = charPool[Random.Next(charPool.Count)];
+            if (!usedChars.Contains(randomChar))
+            {
+                password[index++] = randomChar;
+                usedChars.Add(randomChar);
+            }
+        }
+
+        Shuffle(password);
+
+        return new string(password);
+    }
+    private static void Shuffle(char[] array)
+    {
+        for (int i = array.Length - 1; i > 0; i--)
+        {
+            int j = Random.Next(i + 1);
+            char temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
+//https://www.codewars.com/kata/5b3d5ad43da310743c000056/train/csharp
---
+++ CodeWars6kyuTest/PasswordMaker.cs
@@ -0,0 +1,105 @@
+﻿using CodeWars6kyu;
+
+namespace CodeWars6kyuTest;
+
+public class PasswordMaker
+{
+    [Test]
+    public void TestUpperOnly()
+    {
+        bool upper = true, lower = false, digits = false;
+        int len = 5;
+        string password = PasswordMakerTask.MakePassword(len, upper, lower, digits);
+        Assert.AreEqual(len, password.Length);
+        Assert.IsTrue(password.All(char.IsUpper), "Password should contain only uppercase letters.");
+        Assert.IsTrue(password.Distinct().Count() == len, "Password contains repeated characters.");
+    }
+
+    [Test]
+    public void TestLowerOnly()
+    {
+        bool upper = false, lower = true, digits = false;
+        int len = 5;
+        string password = PasswordMakerTask.MakePassword(len, upper, lower, digits);
+        Assert.AreEqual(len, password.Length);
+        Assert.IsTrue(password.All(char.IsLower), "Password should contain only lowercase letters.");
+        Assert.IsTrue(password.Distinct().Count() == len, "Password contains repeated characters.");
+    }
+
+    [Test]
+    public void TestDigitsOnly()
+    {
+        bool upper = false, lower = false, digits = true;
+        int len = 5;
+        string password = PasswordMakerTask.MakePassword(len, upper, lower, digits);
+        Assert.AreEqual(len, password.Length);
+        Assert.IsTrue(password.All(char.IsDigit), "Password should contain only digits.");
+        Assert.IsTrue(password.Distinct().Count() == len, "Password contains repeated characters.");
+    }
+
+    [Test]
+    public void TestUpperAndLower()
+    {
+        bool upper = true, lower = true, digits = false;
+        int len = 8;
+        string password = PasswordMakerTask.MakePassword(len, upper, lower, digits);
+        Assert.AreEqual(len, password.Length);
+        Assert.IsTrue(password.Any(char.IsUpper) && password.Any(char.IsLower), "Password should contain both uppercase and lowercase letters.");
+        Assert.IsTrue(password.Distinct().Count() == len, "Password contains repeated characters.");
+    }
+
+    [Test]
+    public void TestUpperAndDigits()
+    {
+        bool upper = true, lower = false, digits = true;
+        int len = 10;
+        string password = PasswordMakerTask.MakePassword(len, upper, lower, digits);
+        Assert.AreEqual(len, password.Length);
+        Assert.IsTrue(password.Any(char.IsUpper) && password.Any(char.IsDigit), "Password should contain uppercase letters and digits.");
+        Assert.IsTrue(password.Distinct().Count() == len, "Password contains repeated characters.");
+    }
+
+    [Test]
+    public void TestLowerAndDigits()
+    {
+        bool upper = false, lower = true, digits = true;
+        int len = 12;
+        string password = PasswordMakerTask.MakePassword(len, upper, lower, digits);
+        Assert.AreEqual(len, password.Length);
+        Assert.IsTrue(password.Any(char.IsLower) && password.Any(char.IsDigit), "Password should contain lowercase letters and digits.");
+        Assert.IsTrue(password.Distinct().Count() == len, "Password contains repeated characters.");
+    }
+
+    [Test]
+    public void TestAllFlags()
+    {
+        bool upper = true, lower = true, digits = true;
+        int len = 15;
+        string password = PasswordMakerTask.MakePassword(len, upper, lower, digits);
+        Assert.AreEqual(len, password.Length);
+        Assert.IsTrue(password.Any(char.IsUpper) && password.Any(char.IsLower) && password.Any(char.IsDigit), "Password should contain uppercase letters, lowercase letters, and digits.");
+        Assert.IsTrue(password.Distinct().Count() == len, "Password contains repeated characters.");
+    }
+
+    [Test]
+    public void TestMinLength()
+    {
+        bool upper = true, lower = true, digits = true;
+        int len = 1;
+        string password = PasswordMakerTask.MakePassword(len, upper, lower, digits);
+        Assert.AreEqual(len, password.Length);
+        Assert.IsTrue(password.All(c => char.IsUpper(c) || char.IsLower(c) || char.IsDigit(c)), "Password should contain only alphanumeric characters.");
+        Assert.IsTrue(password.Distinct().Count() == len, "Password contains repeated characters.");
+    }
+
+    [Test]
+    public void TestMaxLength()
+    {
+        bool upper = true, lower = true, digits = true;
+        int len = 50;
+        string password = PasswordMakerTask.MakePassword(len, upper, lower, digits);
+        Assert.AreEqual(len, password.Length);
+        Assert.IsTrue(password.All(c => char.IsUpper(c) || char.IsLower(c) || char.IsDigit(c)), "Password should contain only alphanumeric characters.");
+        Assert.IsTrue(password.Distinct().Count() == len, "Password contains repeated characters.");
+    }
+}
