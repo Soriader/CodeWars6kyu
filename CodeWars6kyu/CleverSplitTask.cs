41076799f0e745399160448d5d2e185235149614
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-02-06 20:05:48 +0100

    [~] Task: Clever split

---
+++ CodeWars6kyu/CleverSplitTask.cs
@@ -0,0 +1,46 @@
+﻿namespace CodeWars6kyu;
+
+public class CleverSplitTask
+{
+    public static string[] CleverSplit(string s)
+    {
+        var boxVorResult = new List<string>();
+        var boxForWords = "";
+        bool isOnBrackets = false;
+
+        foreach (var letter in s)
+        {
+            if (letter == '[')
+            {
+                isOnBrackets = true;
+            }
+
+            if (letter == ' ' && !isOnBrackets)
+            {
+                if (!string.IsNullOrEmpty(boxForWords))
+                {
+                    boxVorResult.Add(boxForWords);
+                    boxForWords = "";
+                }
+                continue;
+            }
+
+            boxForWords += letter;
+
+            if (letter == ']')
+            {
+                isOnBrackets = false;
+                boxVorResult.Add(boxForWords);
+                boxForWords = "";
+            }
+        }
+
+        if (!string.IsNullOrEmpty(boxForWords))
+        {
+            boxVorResult.Add(boxForWords);
+        }
+
+        return boxVorResult.ToArray();
+    }
+}
+//https://www.codewars.com/kata/5992e11d6ca73b38d50000f0/train/csharp
---
+++ CodeWars6kyuTest/CleverSplits.cs
@@ -0,0 +1,24 @@
+﻿namespace CodeWars6kyuTest;
+
+using CodeWars6kyu;
+
+public class CleverSplits
+{
+    [TestFixture]
+    public class CleverSplitTest
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData("Buy a !car [!red green !white] [cheap or !new]").Returns(new string[] {"Buy", "a", "!car", "[!red green !white]", "[cheap or !new]"});
+                yield return new TestCaseData("!Learning !C# is [a joy]").Returns(new string[] {"!Learning", "!C#", "is", "[a joy]"});
+                yield return new TestCaseData("[Cats and dogs] are !beautiful and [cute]").Returns(new string[] {"[Cats and dogs]", "are", "!beautiful", "and", "[cute]"});
+            }
+        }
+  
+        [Test, TestCaseSource("testCases")]
+        public string[] Test(string s) =>
+            CleverSplitTask.CleverSplit(s);
+    }
+}
