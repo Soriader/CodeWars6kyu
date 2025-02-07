bdd4058a4ebf776cbfaed076887f37071e6bc205
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-02-07 18:01:00 +0100

    [~] Task: Cuckoo Clock

---
+++ CodeWars6kyu/CuckooClockTask.cs
@@ -0,0 +1,52 @@
+﻿namespace CodeWars6kyu;
+
+public class CuckooClockTask
+{
+    public static string CuckooClock(string inputTime, int chimes)
+    {
+        int howManyChimes = 0;
+
+        var actualTime = inputTime.Split(':');
+        int startHour = int.Parse(actualTime[0]);
+        int startMinutes = int.Parse(actualTime[1]);
+
+        if (startMinutes == 0)
+        {
+            howManyChimes += startHour;
+        }
+        else if (startMinutes % 15 == 0)
+        {
+            howManyChimes++;
+        }
+
+        while (howManyChimes < chimes)
+        {
+            startMinutes++;
+
+            if (startMinutes == 60)
+            {
+                startMinutes = 0;
+                startHour++;
+                if (startHour > 12)
+                {
+                    startHour = 1;
+                }
+                howManyChimes += startHour;
+            }
+            else if (startMinutes % 15 == 0)
+            {
+                howManyChimes++;
+            }
+        }
+
+        if (startMinutes == 0)
+        {
+            return $"{startHour:D2}:00";
+        }
+        else
+        {
+            return $"{startHour:D2}:{startMinutes:D2}";
+        }
+    }
+}
+//https://www.codewars.com/kata/656e4602ee72af0017e37e82/train/csharp
---
+++ CodeWars6kyuTest/CuckooClock.cs
@@ -0,0 +1,58 @@
+﻿namespace CodeWars6kyuTest;
+
+using CodeWars6kyu;
+
+public class CuckooClock
+{
+        [Test, Order(1)]
+    public void SimpleTests()
+    {
+          List<string> initialTimes = new List<string> { "07:22", "12:22", "01:30", "04:01", "03:38" };
+          List<int> chimes = new List<int> { 1, 2, 2, 10, 19 };
+          List<string> expectedTimes = new List<string> { "07:30", "12:45", "01:45", "05:30", "06:00" };
+
+          for (int i = 0; i < initialTimes.Count; i++)
+          {
+              Assert.That(CuckooClockTask.CuckooClock(initialTimes[i], chimes[i]), Is.EqualTo(expectedTimes[i]));
+          }
+    }
+    
+    [Test, Order(2)]
+    public void HourTests() // Test case where the starting time is 00, i.e. on the hour
+    {
+          List<string> initialTimes = new List<string> { "10:00", "10:00", "10:00", "10:00", "10:00" };
+          List<int> chimes = new List<int> { 1, 10, 11, 13, 20 };
+          List<string> expectedTimes = new List<string> { "10:00", "10:00", "10:15", "10:45", "11:00" };
+
+          for (int i = 0; i < initialTimes.Count; i++)
+          {
+              Assert.That(CuckooClockTask.CuckooClock(initialTimes[i], chimes[i]), Is.EqualTo(expectedTimes[i]));
+          }
+    }
+  
+    [Test, Order(3)]
+    public void TwelveTests() // Test going from twelve to one
+    {
+          List<string> initialTimes = new List<string> { "12:30", "12:30", "12:30", "12:30", "09:53" };
+          List<int> chimes = new List<int> { 1, 2, 3, 4, 50 };
+          List<string> expectedTimes = new List<string> { "12:30", "12:45", "01:00", "01:15", "02:30" };
+
+          for (int i = 0; i < initialTimes.Count; i++)
+          {
+              Assert.That(CuckooClockTask.CuckooClock(initialTimes[i], chimes[i]), Is.EqualTo(expectedTimes[i]));
+          }
+    }
+  
+    [Test, Order(4)]
+    public void AroundTheClockTests() // Test going more than 12 hours ahead
+    {
+          List<string> initialTimes = new List<string> { "08:17", "08:17", "08:17", "08:17", "08:17" };
+          List<int> chimes = new List<int> { 113, 114, 115, 150, 200 };
+          List<string> expectedTimes = new List<string> { "08:00", "08:15", "08:30", "11:00", "05:45" };
+
+          for (int i = 0; i < initialTimes.Count; i++)
+          {
+              Assert.That(CuckooClockTask.CuckooClock(initialTimes[i], chimes[i]), Is.EqualTo(expectedTimes[i]));
+          }
+    }
+}
