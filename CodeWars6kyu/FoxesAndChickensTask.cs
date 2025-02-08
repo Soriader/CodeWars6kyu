7356f5d41662ee07059a4c4eba67c3ddef1b9134
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-02-08 20:35:54 +0100

    [~] Task: The Hunger Games - Foxes and Chickens

---
+++ CodeWars6kyu/FoxesAndChickensTask.cs
@@ -0,0 +1,65 @@
+﻿namespace CodeWars6kyu;
+
+public class FoxesAndChickensTask
+{
+    public static string HungryFoxes(string farm)
+    {
+        string result = "";
+        bool cage = false;
+        bool foxOutside = false;
+        string boxForChicken = "";
+
+        for (int i = 0; i < farm.Length; i++)
+        {
+            if (farm[i] == '[')
+            {
+                cage = true;
+            }
+            else if (farm[i] == ']')
+            {
+                cage = false;
+            }
+            else if (!cage && farm[i] == 'F')
+            {
+                foxOutside = true;
+            }
+        }
+
+        cage = false; 
+        
+        for (int i = 0; i < farm.Length; i++)
+        {
+            if (farm[i] == '[')
+            {
+                cage = true;
+                result += "[";
+                boxForChicken = ""; 
+            }
+            else if (farm[i] == ']')
+            {
+                cage = false;
+                if (boxForChicken.Contains('F'))
+                {
+                    boxForChicken = boxForChicken.Replace('C', '.');
+                }
+                result += boxForChicken + "]";
+            }
+            else if (cage)
+            {
+                boxForChicken += farm[i];
+            }
+            else if (foxOutside && farm[i] == 'C')
+            {
+                result += ".";
+            }
+            else
+            {
+                result += farm[i];
+            }
+        }
+
+        return result;
+    }
+
+}
+//https://www.codewars.com/kata/591144f42e6009675300001f/train/csharp
---
+++ CodeWars6kyuTest/FoxesAndChickens.cs
@@ -0,0 +1,31 @@
+﻿namespace CodeWars6kyuTest;
+
+using CodeWars6kyu;
+
+
+public class FoxesAndChickens
+{
+    [Test]
+    public void Example1()
+    {
+        string before = "CCC[CCC]FCC[CCCCC]CFFFF[CCC]FFFF";
+        string after = "...[CCC]F..[CCCCC].FFFF[CCC]FFFF";
+        Assert.That(FoxesAndChickensTask.HungryFoxes(before), Is.EqualTo(after));
+    }
+
+    [Test]
+    public void Example2()
+    {
+        string before = "...[CCC]...[CCCFC].....[CCC]....";
+        string after = "...[CCC]...[...F.].....[CCC]....";
+        Assert.That(FoxesAndChickensTask.HungryFoxes(before), Is.EqualTo(after));
+    }
+    
+    [Test]
+    public void Example3()
+    {
+        string before = "CCC[CCC]FCC[CCCFC]CFFFF[CCC]FFFF";
+        string after = "...[CCC]F..[...F.].FFFF[CCC]FFFF";
+        Assert.That(FoxesAndChickensTask.HungryFoxes(before), Is.EqualTo(after));
+    }
+}
