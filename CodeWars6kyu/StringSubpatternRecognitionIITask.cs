5117a351643077cb46fcd0ab448fe2cb8668b30a
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-02-09 21:16:47 +0100

    [~] Task: String subpattern recognition II

---
+++ CodeWars6kyu/StringSubpatternRecognitionIITask.cs
@@ -0,0 +1,37 @@
+﻿namespace CodeWars6kyu;
+
+public class StringSubpatternRecognitionIITask
+{
+    public static bool HasSubpattern(string str)
+    {
+        char[] arr = str.ToCharArray();
+            
+        Dictionary<string, int> counter = new Dictionary<string, int>();
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (counter.ContainsKey(arr[i].ToString()))
+            {
+                counter[arr[i].ToString()]++;
+            }
+            else
+            {
+                counter.TryAdd(arr[i].ToString(), 1);
+            }
+        }
+
+        var counterList = counter.Values.ToList();
+
+        for (int i = 2; i <= str.Length; i++)
+        {
+            if (counterList.All(count => count % i == 0))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+//https://www.codewars.com/kata/5a4a391ad8e145cdee0000c4/train/csharp
---
+++ CodeWars6kyuTest/StringSubpatternRecognitionII.cs
@@ -0,0 +1,29 @@
+﻿namespace CodeWars6kyuTest;
+
+using CodeWars6kyu;
+
+public class StringSubpatternRecognitionII
+{
+    [Test]
+    public void StringSubpatternRecognitionIITest()
+    {
+        Assert.That(StringSubpatternRecognitionIITask.HasSubpattern("a"), Is.EqualTo(false));
+        Assert.That(StringSubpatternRecognitionIITask.HasSubpattern("AA"), Is.EqualTo(true));
+        Assert.That(StringSubpatternRecognitionIITask.HasSubpattern("444"), Is.EqualTo(true));
+        Assert.That(StringSubpatternRecognitionIITask.HasSubpattern("aaaa"), Is.EqualTo(true));
+        Assert.That(StringSubpatternRecognitionIITask.HasSubpattern("abcd"), Is.EqualTo(false));
+        Assert.That(StringSubpatternRecognitionIITask.HasSubpattern("babababababababa"), Is.EqualTo(true));
+        Assert.That(StringSubpatternRecognitionIITask.HasSubpattern("ababababa"), Is.EqualTo(false));
+        Assert.That(StringSubpatternRecognitionIITask.HasSubpattern("123a123a123a"), Is.EqualTo(true));
+        Assert.That(StringSubpatternRecognitionIITask.HasSubpattern("123A123a123a"), Is.EqualTo(false));
+        Assert.That(StringSubpatternRecognitionIITask.HasSubpattern("12aa13a21233"), Is.EqualTo(true));
+        Assert.That(StringSubpatternRecognitionIITask.HasSubpattern("12aa13a21233A"), Is.EqualTo(false));
+        Assert.That(StringSubpatternRecognitionIITask.HasSubpattern("abcdabcaccd"), Is.EqualTo(false));
+        Assert.That(StringSubpatternRecognitionIITask.HasSubpattern("aaaabb"), Is.EqualTo(true));
+        Assert.That(StringSubpatternRecognitionIITask.HasSubpattern("abbb"), Is.EqualTo(false));
+        Assert.That(StringSubpatternRecognitionIITask.HasSubpattern("aabbbbbbaa"), Is.EqualTo(true));
+        Assert.That(StringSubpatternRecognitionIITask.HasSubpattern("bbabbaaabbaaaabb"), Is.EqualTo(true));
+        Assert.That(StringSubpatternRecognitionIITask.HasSubpattern("aaabbbccccdddddd"), Is.EqualTo(false));
+        Assert.That(StringSubpatternRecognitionIITask.HasSubpattern("aaabbbccccdddddddd"), Is.EqualTo(false));
+    }
+}
