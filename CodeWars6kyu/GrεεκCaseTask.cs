892e6b605bb5a4ee1d6cb641f1e53e39fd4aa5f3
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-02-22 18:21:54 +0100

    [~] Task: (L33T + Grεεκ) Case

---
+++ CodeWars6kyu/GrεεκCaseTask.cs
@@ -0,0 +1,42 @@
+﻿namespace CodeWars6kyu;
+
+public class GrεεκCaseTask
+{
+    public static string GreekL33t(string str)
+    {
+
+        Dictionary<char, char> lowerCaseMap = new Dictionary<char, char>
+        {
+            {'a', 'α'}, {'b', 'β'}, {'d', 'δ'}, {'e', 'ε'}, {'i', 'ι'},
+            {'k', 'κ'}, {'n', 'η'}, {'o', 'θ'}, {'p', 'ρ'}, {'r', 'π'},
+            {'t', 'τ'}, {'u', 'μ'}, {'v', 'υ'}, {'w', 'ω'}, {'x', 'χ'},
+            {'y', 'γ'}
+        };
+
+        string result = "";
+        var check = str.ToLower();
+        
+        foreach (char letter in check)
+        {
+            if (char.IsLetter(letter))
+            {
+                if (lowerCaseMap.ContainsKey(letter))
+                {
+                    result += lowerCaseMap[letter];
+                }
+                else
+                {
+                    result += letter;
+                }
+            }
+            else
+            {
+                result += letter;
+            }
+            
+        }
+        
+        return result;
+    }
+}
+//https://www.codewars.com/kata/556025c8710009fc2d000011/train/csharp
---
+++ CodeWars6kyuTest/GrεεκCase.cs
@@ -0,0 +1,15 @@
+﻿namespace CodeWars6kyuTest;
+
+using CodeWars6kyu;
+
+public class GrεεκCase
+{
+    [Test]
+    public void GrεεκCaseTest()
+    {
+        Assert.That(GrεεκCaseTask.GreekL33t("codewars"), Is.EqualTo("cθδεωαπs"));
+        Assert.That(GrεεκCaseTask.GreekL33t("kata"), Is.EqualTo("κατα"));
+        Assert.That(GrεεκCaseTask.GreekL33t("kumite"), Is.EqualTo("κμmιτε"));
+        Assert.That(GrεεκCaseTask.GreekL33t("greekleet"), Is.EqualTo("gπεεκlεετ"));
+    }
+}
