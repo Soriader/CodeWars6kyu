e0b44408b5f1ecb62c5322b4d3bc7f3248d95928
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-02-24 19:48:15 +0100

    [~] Task: Complete The Pattern #16

---
+++ CodeWars6kyu/CompleteThePattern16Task.cs
@@ -0,0 +1,60 @@
+﻿namespace CodeWars6kyu;
+
+public class CompleteThePattern16Task
+{
+    public static string Pattern(int n)
+    {
+        if (n < 1)
+        {
+            return "";
+        }
+
+        if (n == 1)
+        {
+            return "1";
+        }
+        
+        string result = "";
+        int boxForSecondSequence = 0;
+        string boxForFirstSequence = "";
+
+        if (n < 10)
+        {
+            boxForFirstSequence = $"{n}";
+            boxForSecondSequence = n;
+        }
+        else
+        {
+            boxForFirstSequence = n.ToString().Last().ToString();
+            boxForSecondSequence = int.Parse(boxForFirstSequence);
+        }
+        
+        int iterator = 0;
+        int iteratorToSecondSequence = n - 1;
+            
+        while (iterator != n)
+        {
+            result += boxForFirstSequence 
+                      + string.Concat(Enumerable.Repeat(boxForSecondSequence, iteratorToSecondSequence)) 
+                      + "\n";
+
+
+            boxForSecondSequence--;
+            iteratorToSecondSequence--;
+            
+            if (boxForSecondSequence < 0)
+            {
+                boxForSecondSequence = 9;
+            }
+            
+            boxForFirstSequence += boxForSecondSequence;
+            
+            
+            iterator++;
+        }
+        
+        
+        return result.TrimEnd('\n');
+    }
+}
+//https://www.codewars.com/kata/55ae997d1c40a199e6000018/train/csharp
