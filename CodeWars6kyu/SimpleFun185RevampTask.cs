b977e9a14419ab28447b3704e5d0ef410c608237
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-02-26 13:41:40 +0100

    [~] Task: Simple Fun #185: Revamp

---
+++ CodeWars6kyu/SimpleFun185RevampTask.cs
@@ -0,0 +1,22 @@
+﻿namespace CodeWars6kyu;
+
+public class SimpleFun185RevampTask
+{
+    public string Revamp(string s)
+    {
+        var words = s.Split(" ")
+            .Select(word => new string(word.OrderBy(c => c).ToArray()))
+            .ToArray();
+        List<string> alphabetWords = new List<string>(words);
+        
+        var sortedWords = words.OrderBy(word => word.Sum(c => (int)c)) 
+            .ThenBy(word => word.Length)           
+            .ThenBy(word => word)                  
+            .ToList();
+
+        string result = string.Join(" ", sortedWords);
+
+        return result;
+    }
+}
+//https://www.codewars.com/kata/58bcfe1e23fee9fd95000007/train/csharp
---
+++ CodeWars6kyuTest/SimpleFun185Revamp.cs
@@ -0,0 +1,19 @@
+﻿namespace CodeWars6kyuTest;
+
+using CodeWars6kyu;
+
+public class SimpleFun185Revamp
+{
+    [Test]
+    public void SimpleFun185RevampTest(){
+        var kata=new SimpleFun185RevampTask();
+
+        Assert.That(kata.Revamp("batman is bruce wayne"), Is.EqualTo("is bceru aenwy aabmnt"));
+
+        Assert.That(kata.Revamp("peter parker is spiderman"), Is.EqualTo("is eeprt aekprr adeimnprs"));
+
+        Assert.That(kata.Revamp("codewars is great"), Is.EqualTo("is aegrt acdeorsw"));
+
+        Assert.That(kata.Revamp("airplanes in the night sky"), Is.EqualTo("in eht ksy ghint aaeilnprs"));
+    }
+}
