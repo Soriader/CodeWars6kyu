68bb3ce0cc97359b786dd7d73b24483af5517daa
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-03-02 20:09:22 +0100

    [~] Task: PatternCraft - Decorator

---
+++ CodeWars6kyu/PatternCraftDecoratorTask.cs
@@ -0,0 +1,46 @@
+﻿namespace CodeWars6kyu;
+
+public interface IMarine
+{
+    int Damage { get; }
+    int Armor { get; }
+}
+
+public class Marine : IMarine
+{
+    public Marine(int damage, int armor)
+    {
+        Damage = damage;
+        Armor = armor;
+    }
+
+    public int Damage { get; }
+    public int Armor { get; }
+}
+
+public class MarineWeaponUpgrade : IMarine
+{
+    private readonly IMarine marine;
+
+    public MarineWeaponUpgrade(IMarine marine)
+    {
+        this.marine = marine;
+    }
+
+    public int Damage => marine.Damage + 1;
+    public int Armor => marine.Armor;
+}
+
+public class MarineArmorUpgrade : IMarine
+{
+    private readonly IMarine marine;
+
+    public MarineArmorUpgrade(IMarine marine)
+    {
+        this.marine = marine;
+    }
+
+    public int Damage => marine.Damage;
+    public int Armor => marine.Armor + 1;
+}
+//https://www.codewars.com/kata/5682e545fb263ecf7b000069/train/csharp
---
+++ CodeWars6kyuTest/PatternCraftDecorator.cs
@@ -0,0 +1,36 @@
+﻿namespace CodeWars6kyuTest;
+
+using CodeWars6kyu;
+
+public class PatternCraftDecorator
+{
+    [Test, Order(1)]
+    public void _0_SingleUpgrade()
+    {
+        IMarine marine = new Marine(10, 1);
+
+        Assert.That(new MarineWeaponUpgrade(marine).Damage, Is.EqualTo(11));
+        Assert.That(new MarineWeaponUpgrade(marine).Damage, Is.EqualTo(11));
+    }
+
+    [Test, Order(2)]
+    public void _1_DoubleUpgrade()
+    {
+        IMarine marine = new Marine(15, 1);
+        marine = new MarineWeaponUpgrade(marine);
+        marine = new MarineWeaponUpgrade(marine);
+
+        Assert.That(marine.Damage, Is.EqualTo(17));
+    }
+
+    [Test, Order(3)]
+    public void _2_TripleUpgrade()
+    {
+        IMarine marine = new Marine(20, 1);
+        marine = new MarineWeaponUpgrade(marine);
+        marine = new MarineWeaponUpgrade(marine);
+        marine = new MarineWeaponUpgrade(marine);
+
+        Assert.That(marine.Damage, Is.EqualTo(23));
+    }
+}
