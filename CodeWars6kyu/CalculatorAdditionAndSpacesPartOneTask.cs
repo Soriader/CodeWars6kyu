d873af08e38c12b9a7254a8fbef6245b1831be0e
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-03-24 11:29:40 +0100

    [~] Task: Calculator (Addition and Spaces) [Part 1]

---
+++ CodeWars6kyu/CalculatorAdditionAndSpacesPartOneTask.cs
@@ -0,0 +1,33 @@
+﻿namespace CodeWars6kyu;
+
+public class CalculatorAdditionAndSpacesPartOneTask
+{
+    public static int Calculate(string str)
+    {
+        var result = new List<int>();
+        
+        str = str.Replace(" ", "");
+        var boxForNumbers = "";
+        
+        foreach (var c in str)
+        {
+            if (c == '+')
+            {
+                result.Add(int.Parse(boxForNumbers));
+                boxForNumbers = "";
+            }
+            else
+            {
+                boxForNumbers += c;
+            }
+        }
+
+        if (boxForNumbers.Length > 0)
+        {
+            result.Add(int.Parse(boxForNumbers));
+        }
+        
+        return result.Sum();
+    }
+}
+//https://www.codewars.com/kata/59512d72944ca1feb000013d/train/csharp
---
+++ CodeWars6kyuTest/CalculatorAdditionAndSpacesPartOne.cs
@@ -0,0 +1,14 @@
+﻿namespace CodeWars6kyuTest;
+
+using CodeWars6kyu;
+
+public class CalculatorAdditionAndSpacesPartOne
+{
+    [Test]
+    public void CalculatorAdditionAndSpacesPartOneTest()
+    {
+        Assert.That(CalculatorAdditionAndSpacesPartOneTask.Calculate("1+2+3"), Is.EqualTo(6));
+        Assert.That(CalculatorAdditionAndSpacesPartOneTask.Calculate("1+2 3"), Is.EqualTo(24));
+        Assert.That(CalculatorAdditionAndSpacesPartOneTask.Calculate("1 2 3"), Is.EqualTo(123));
+    }
+}
