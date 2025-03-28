3295d46baf40019c3fb19fd770d5a9ad501ba5d4
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-03-28 21:56:33 +0100

    [~] Task: Conversion between Kilobyte and KibiByte

---
+++ CodeWars6kyu/KilobyteAndKibiByteTask.cs
@@ -0,0 +1,54 @@
+﻿using System.Globalization;
+
+namespace CodeWars6kyu;
+
+public class KilobyteAndKibiByteTask
+{
+    public static string MemorySizeConversion(string memorySize) 
+    {
+        string[] parts = memorySize.Split(' ');
+        double value = double.Parse(parts[0], CultureInfo.InvariantCulture);
+        string unit = parts[1];
+    
+        switch (unit)
+        {
+            case "kB":
+            {
+                return $"{(value / 1.024).ToString("0.###", CultureInfo.InvariantCulture)} KiB";
+            }
+            case "KiB":
+            {
+                return $"{(value * 1.024).ToString("0.###", CultureInfo.InvariantCulture)} kB";
+            }
+            case "MB":
+            {
+                return $"{(value / 1.048576).ToString("0.###", CultureInfo.InvariantCulture)} MiB";
+            }
+            case "MiB":
+            {
+                return $"{(value * 1.048576).ToString("0.###", CultureInfo.InvariantCulture)} MB";
+            }
+            case "GB":
+            {
+                return $"{(value / 1.073741824).ToString("0.###", CultureInfo.InvariantCulture)} GiB";
+            }
+            case "GiB":
+            {
+                return $"{(value * 1.073741824).ToString("0.###", CultureInfo.InvariantCulture)} GB";
+            }
+            case "TB":
+            {
+                return $"{(value / 1.099511627776).ToString("0.###", CultureInfo.InvariantCulture)} TiB";
+            }
+            case "TiB":
+            {
+                return $"{(value * 1.099511627776).ToString("0.###", CultureInfo.InvariantCulture)} TB";
+            }
+            default:
+            {
+                return memorySize;
+            }
+        }
+    }
+}
+//https://www.codewars.com/kata/5a115ff080171f9651000046/train/csharp
---
+++ CodeWars6kyuTest/KilobyteAndKibiByte.cs
@@ -0,0 +1,19 @@
+﻿namespace CodeWars6kyuTest;
+
+using CodeWars6kyu;
+
+public class KilobyteAndKibiByte
+{
+    [Test]
+    public void KilobyteAndKibiByteTest()
+    {
+        Assert.That(KilobyteAndKibiByteTask.MemorySizeConversion("1 KiB"), Is.EqualTo("1.024 kB"));
+        Assert.That(KilobyteAndKibiByteTask.MemorySizeConversion("1 kB"), Is.EqualTo("0.977 KiB"));
+        Assert.That(KilobyteAndKibiByteTask.MemorySizeConversion("1 MiB"), Is.EqualTo("1.049 MB"));
+        Assert.That(KilobyteAndKibiByteTask.MemorySizeConversion("1 MB"), Is.EqualTo("0.954 MiB"));
+        Assert.That(KilobyteAndKibiByteTask.MemorySizeConversion("1 GB"), Is.EqualTo("0.931 GiB"));
+        Assert.That(KilobyteAndKibiByteTask.MemorySizeConversion("1 GiB"), Is.EqualTo("1.074 GB"));
+        Assert.That(KilobyteAndKibiByteTask.MemorySizeConversion("1 TB"), Is.EqualTo("0.909 TiB"));
+        Assert.That(KilobyteAndKibiByteTask.MemorySizeConversion("1 TiB"), Is.EqualTo("1.1 TB"));
+    }
+}
