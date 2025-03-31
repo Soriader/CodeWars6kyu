c900e0c398cf2094e96b62b94dd7346fde15de83
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-03-31 20:53:42 +0200

    [~]Task: Fun with lists: map

---
+++ CodeWars6kyu/FunWithListsMapTask.cs
@@ -0,0 +1,44 @@
+﻿
+namespace CodeWars6kyu;
+
+public class FunWithListsMapTask
+{
+    public static Node<T2> Map<T, T2>(Node<T> head, Func<T, T2> f) 
+    {
+        if (head == null)
+        {
+            return null;
+        }
+        
+        Node<T2> newHead = new Node<T2>(f(head.data));
+        Node<T2> currentNew = newHead;
+        Node<T> currentOriginal = head.next;
+        
+        while (currentOriginal != null)
+        {
+            currentNew.next = new Node<T2>(f(currentOriginal.data));
+            currentNew = currentNew.next;
+            currentOriginal = currentOriginal.next;
+        }
+        
+        return newHead;
+    }
+}
+
+public class Node<T>
+{
+    public T data;
+    public Node<T> next;
+
+    public Node(T data)  
+    {
+        this.data = data;
+    }
+
+    public Node(T data, Node<T> next) 
+    {
+        this.data = data;
+        this.next = next;
+    }
+}
+//https://www.codewars.com/kata/58259d9062cfb45e1a00006b/train/csharp
