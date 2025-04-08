4e5c1c7682b0ce00b2e10a40c22e8ca90736b13e
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-04-08 17:38:21 +0200

    [~] Task: Paths in the Grid

---
+++ CodeWars6kyu/PathsInTheGridTask.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+
+namespace CodeWars6kyu;
+
+public class PathsInTheGridTask
+{
+    public static BigInteger NumberOfRoutes(int m, int n)
+    {
+        int smaller = Math.Min(m, n);
+        int total = m + n;
+        
+        BigInteger result = 1;
+        
+        for (int i = 1; i <= smaller; i++)
+        {
+            result = result * (total - smaller + i) / i;
+        }
+        
+        return result;        
+    }
+}
+//https://www.codewars.com/kata/56a127b14d9687bba200004d/train/csharp
---
+++ CodeWars6kyuTest/PathsInTheGrid.cs
@@ -0,0 +1,25 @@
+﻿namespace CodeWars6kyuTest;
+
+using CodeWars6kyu;
+using System.Numerics;
+
+public class PathsInTheGrid
+{
+    [Test, Order(1)]
+    public void FirstTest()
+    {
+        Assert.That(PathsInTheGridTask.NumberOfRoutes(1, 1), Is.EqualTo(BigInteger.Parse("2")));
+    }
+
+    [Test, Order(2)]
+    public void SecondTest()
+    {
+        Assert.That(PathsInTheGridTask.NumberOfRoutes(5, 1), Is.EqualTo(BigInteger.Parse("6")));
+    }
+
+    [Test, Order(3)]
+    public void ThirdTest()
+    {
+        Assert.That(PathsInTheGridTask.NumberOfRoutes(3, 4), Is.EqualTo(BigInteger.Parse("35")));
+    }
+}
