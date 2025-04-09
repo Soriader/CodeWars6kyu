88f16b5da4d7aa238629705b4e5ead953a45aa9e
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-04-09 16:24:19 +0200

    [~] Task: "com", "gov", "org" first

---
+++ CodeWars6kyu/ComGovOrgFirstTask.cs
@@ -0,0 +1,29 @@
+﻿namespace CodeWars6kyu;
+
+public class ComGovOrgFirstTask
+{
+    public static IEnumerable<string> OrderByDomain(IEnumerable<string> source)
+    {
+        var priorityTlds = new List<string> { "com", "gov", "org" };
+
+        var parsed = source
+            .Select(uriStr => new
+            {
+                Uri = uriStr,
+                Tld = new Uri(uriStr).Host.Split('.').Last()
+            });
+
+        var priorityGroup = parsed
+            .Where(x => priorityTlds.Contains(x.Tld))
+            .OrderBy(x => priorityTlds.IndexOf(x.Tld));
+
+        var otherGroup = parsed
+            .Where(x => !priorityTlds.Contains(x.Tld))
+            .OrderBy(x => x.Tld);
+
+        return priorityGroup
+            .Concat(otherGroup)
+            .Select(x => x.Uri);
+    }
+}
+//https://www.codewars.com/kata/57f21fcd69e09cb0d2000088/train/csharp
---
+++ CodeWars6kyuTest/ComGovOrgFirst.cs
@@ -0,0 +1,37 @@
+﻿namespace CodeWars6kyuTest;
+
+using CodeWars6kyu;
+
+public class ComGovOrgFirst
+{
+    [Test]
+    public void SimpleTests()
+    {    
+        var source = new[] {
+            "http://www.google.en/?x=jsdfkj",
+            "http://www.google.de/?x=jsdfkj",
+            "http://www.google.com/?x=jsdfkj",
+            "http://www.google.com/?x=jsdfkj",
+            "http://www.google.org/?x=jsdfkj",
+            "http://www.google.gov/?x=jsdfkj",
+            "http://www.google.acom/?x=jsdfkj",
+            "http://www.google.govt/?x=jsdfkj",
+        };
+        var result = ComGovOrgFirstTask.OrderByDomain(source);
+
+        //SortingTaskUtils.AreDomainsEqual(result, source);
+      
+        /*
+           should be sorted to [
+             "http://www.google.com/?x=jsdfkj",
+             "http://www.google.com/?x=jsdfkj",
+             "http://www.google.acom/?x=jsdfkj",
+             "http://www.google.gov/?x=jsdfkj",
+             "http://www.google.govt/?x=jsdfkj",
+             "http://www.google.org/?x=jsdfkj",
+             "http://www.google.de/?x=jsdfkj",
+             "http://www.google.en/?x=jsdfkj",
+           ]
+        */
+    }
+}
