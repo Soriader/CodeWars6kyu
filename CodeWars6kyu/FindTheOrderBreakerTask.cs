19780696e60e04da11feea54cce82cf6859e2823
NewLavoisier <kacperjwawrzyniak@gmail.com>
2025-04-11 16:17:40 +0200

    [~] Taask: Find the Order Breaker

---
+++ CodeWars6kyu/FindTheOrderBreakerTask.cs
@@ -0,0 +1,35 @@
+﻿namespace CodeWars6kyu;
+
+public class FindTheOrderBreakerTask
+{
+    public static int OrderBreaker(int[] input)
+    {
+        if (input.Length >= 2 && input[0] > input[1])
+        {
+            return input[0];
+        }
+
+        for (int i = 1; i < input.Length - 1; i++)
+        {
+            if (input[i] > input[i + 1])
+            {
+                if (input[i - 1] < input[i + 1])
+                {
+                    return input[i];
+                }
+                else
+                {
+                    return input[i + 1];
+                }
+            }
+        }
+
+        if (input.Length >= 2 && input[input.Length - 2] > input[input.Length - 1])
+        {
+            return input[input.Length - 1];
+        }
+
+        return 0;
+    }
+}
+//https://www.codewars.com/kata/5fc2a4b9bb2de30012c49609/train/csharp
---
+++ CodeWars6kyuTest/FindTheOrderBreaker.cs
@@ -0,0 +1,33 @@
+﻿namespace CodeWars6kyuTest;
+
+using CodeWars6kyu;
+
+
+public class FindTheOrderBreaker
+{
+    List<(List<int>, int)> tests = new List<(List<int>, int)>() {
+        (new List<int>() { 2, 1 }, 2 ),
+        (new List<int>() { 0, -10 }, 0),
+        (new List<int>() { 15, 0 }, 15 ),
+        (new List<int>() { 1, 3, 2 }, 3 ),
+        (new List<int>() { 1, 0, 2 }, 1 ),
+        (new List<int>() { 1, 2, 0, 3, 4 }, 0 ),
+        (new List<int>() { 1, 2, 3, 4, -1 }, -1 ),
+        (new List<int>() { -11, 5, 0, 3, 4 }, 5 ),
+        (new List<int>() { -11, -5, 0, -2, 4 }, 0 ),
+        (new List<int>() { 5, 7, 6, 8, 9, 10 }, 7 ),
+        (new List<int>() { 1, 2, 3, 4, 17, 5, 6, 7, 8 }, 17 ),
+        (new List<int>() { 19, 27, 33, 34, 112, 578, 116, 170, 800 }, 578 ),
+        (new List<int>() { 105, 110, 111, 112, 114, 113, 115 }, 114 ),
+        (new List<int>() { 1001, 991, 992, 993, 994, 995, 996 }, 1001 ),
+    };
+
+    [Test]
+    public void FindTheOrderBreakerTest() 
+    {
+        foreach (var test in tests) 
+        {
+            Assert.That(FindTheOrderBreakerTask.OrderBreaker(test.Item1.ToArray()), Is.EqualTo(test.Item2));
+        }
+    }
+}
